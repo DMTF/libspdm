630e358c6cdbff5c8c5fd00514ef8a0ef10dc0ce
Jiewen Yao <jiewen.yao@intel.com>
2021-07-01 14:31:37 +0800

    clean up mbedtls
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- os_stub/mbedtlslib/include/mbedtls/config.h
+++ os_stub/mbedtlslib/include/mbedtls/config.h
@@ -1,4 +1,14 @@
 /**
+    Copyright Notice:
+    Copyright 2021 DMTF. All rights reserved.
+    License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
+**/
+
+/** @file
+  mbedtls customized configuration file.
+**/
+
+/**
  * \file config.h
  *
  * \brief Configuration options (set of defines)
