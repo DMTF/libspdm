1df7dc26ef3ee378b9a983c084c6e17d5bf7c44d
Steven Bellock <steven.bellock@gmail.com>
2021-08-22 10:16:26 -0700

    Enable unit testing in workflow.
    test_spdm_requester and test_spdm_responder now return the test status as an exit code. cmake.yml runs the two tests as part of the build process.

--- unit_test/test_spdm_requester/test_spdm_requester.c
+++ unit_test/test_spdm_requester/test_spdm_requester.c
@@ -1,7 +1,7 @@
 /**
-    Copyright Notice:
-    Copyright 2021 DMTF. All rights reserved.
-    License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
+	Copyright Notice:
+	Copyright 2021 DMTF. All rights reserved.
+	License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
 **/
 
 #include "spdm_unit_test.h"
@@ -23,30 +23,59 @@ int spdm_requester_end_session_test_main(void);
 
 int main(void)
 {
-	spdm_requester_get_version_test_main();
+	int return_value = 0;
 
-	spdm_requester_get_capabilities_test_main();
+	if (spdm_requester_get_version_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_negotiate_algorithms_test_main();
+	if (spdm_requester_get_capabilities_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_get_digests_test_main();
+	if (spdm_requester_negotiate_algorithms_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_get_certificate_test_main();
+	if (spdm_requester_get_digests_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_challenge_test_main();
+	if (spdm_requester_get_certificate_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_get_measurements_test_main();
+	if (spdm_requester_challenge_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_key_exchange_test_main();
+	if (spdm_requester_get_measurements_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_finish_test_main();
+	if (spdm_requester_key_exchange_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_psk_exchange_test_main();
+	if (spdm_requester_finish_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_psk_finish_test_main();
+	if (spdm_requester_psk_exchange_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_heartbeat_test_main();
+	if (spdm_requester_psk_finish_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_requester_end_session_test_main();
-	return 0;
+	if (spdm_requester_heartbeat_test_main() != 0) {
+		return_value = 1;
+	}
+
+	if (spdm_requester_end_session_test_main() != 0) {
+		return_value = 1;
+	}
+
+	return return_value;
 }
--- unit_test/test_spdm_responder/test_spdm_responder.c
+++ unit_test/test_spdm_responder/test_spdm_responder.c
@@ -1,7 +1,7 @@
 /**
-    Copyright Notice:
-    Copyright 2021 DMTF. All rights reserved.
-    License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
+	Copyright Notice:
+	Copyright 2021 DMTF. All rights reserved.
+	License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
 **/
 
 #include "spdm_unit_test.h"
@@ -24,32 +24,63 @@ int spdm_responder_end_session_test_main(void);
 
 int main(void)
 {
-	spdm_responder_version_test_main();
+	int return_value = 0;
 
-	spdm_responder_capabilities_test_main();
+	if (spdm_responder_version_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_algorithms_test_main();
+	if (spdm_responder_capabilities_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_digests_test_main();
+	if (spdm_responder_algorithms_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_certificate_test_main();
+	if (spdm_responder_digests_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_challenge_auth_test_main();
+	if (spdm_responder_certificate_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_measurements_test_main();
+	if (spdm_responder_challenge_auth_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_respond_if_ready_test_main ();
+	if (spdm_responder_measurements_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_key_exchange_test_main();
+	if (spdm_responder_respond_if_ready_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_finish_test_main();
+	if (spdm_responder_key_exchange_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_psk_exchange_test_main();
+	if (spdm_responder_finish_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_psk_finish_test_main();
+	if (spdm_responder_psk_exchange_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_heartbeat_test_main();
+	if (spdm_responder_psk_finish_test_main() != 0) {
+		return_value = 1;
+	}
 
-	spdm_responder_end_session_test_main();
-	return 0;
+	if (spdm_responder_heartbeat_test_main() != 0) {
+		return_value = 1;
+	}
+
+	if (spdm_responder_end_session_test_main() != 0) {
+		return_value = 1;
+	}
+
+	return return_value;
 }
