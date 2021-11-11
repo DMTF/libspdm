2b623f1189196efe45a03d7d3cc274a26bb3bfae
Xiaohanjlll <hanx.xiao@intel.com>
2021-11-12 01:51:17 +0800

    key_exchange test case
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

---
+++ unit_test/fuzzing/test_spdm_responder_key_exchange/key_exchange.c
@@ -0,0 +1,96 @@
+/**
+    Copyright Notice:
+    Copyright 2021 DMTF. All rights reserved.
+    License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
+**/
+
+#include "spdm_unit_fuzzing.h"
+#include "toolchain_harness.h"
+#include <spdm_device_secret_lib_internal.h>
+#include <spdm_responder_lib_internal.h>
+
+typedef struct {
+	spdm_message_header_t header;
+	uint16 req_session_id;
+	uint16 reserved;
+	uint8 random_data[SPDM_RANDOM_DATA_SIZE];
+	uint8 exchange_data[MAX_DHE_KEY_SIZE];
+	uint16 opaque_length;
+	uint8 opaque_data[MAX_SPDM_OPAQUE_DATA_SIZE];
+} spdm_key_exchange_request_mine_t;
+
+uintn get_max_buffer_size(void)
+{
+	return MAX_SPDM_MESSAGE_BUFFER_SIZE;
+}
+
+spdm_test_context_t m_spdm_responder_key_exchange_test_context = {
+	SPDM_TEST_CONTEXT_SIGNATURE,
+	FALSE,
+};
+
+spdm_key_exchange_request_mine_t m_spdm_key_exchange_request2 = {
+	{ SPDM_MESSAGE_VERSION_11, SPDM_KEY_EXCHANGE,
+	  SPDM_CHALLENGE_REQUEST_NO_MEASUREMENT_SUMMARY_HASH, 0 },
+};
+uintn m_spdm_key_exchange_request2_size = sizeof(spdm_key_exchange_request_t);
+
+void test_spdm_responder_key_exchange(void **State)
+{
+	spdm_test_context_t *spdm_test_context;
+	spdm_context_t *spdm_context;
+	uintn response_size;
+	uint8 response[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+	void *data;
+	uintn data_size;
+
+	spdm_test_context = *State;
+	spdm_context = spdm_test_context->spdm_context;
+	spdm_context->connection_info.connection_state =
+		SPDM_CONNECTION_STATE_NEGOTIATED;
+	spdm_context->connection_info.capability.flags |=
+		SPDM_GET_CAPABILITIES_REQUEST_FLAGS_KEY_EX_CAP;
+	spdm_context->local_context.capability.flags |=
+		SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_KEY_EX_CAP;
+	spdm_context->connection_info.algorithm.base_hash_algo =
+		SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA_256;
+	spdm_context->connection_info.algorithm.base_asym_algo =
+		SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA_256;
+	spdm_context->connection_info.algorithm.measurement_spec =
+		SPDM_MEASUREMENT_BLOCK_HEADER_SPECIFICATION_DMTF;
+	spdm_context->connection_info.algorithm.measurement_hash_algo =
+		SPDM_ALGORITHMS_MEASUREMENT_HASH_ALGO_TPM_ALG_SHA_256;
+	spdm_context->connection_info.algorithm.dhe_named_group =
+		SPDM_ALGORITHMS_DHE_NAMED_GROUP_SECP_256_R1;
+	spdm_context->connection_info.algorithm.aead_cipher_suite =
+		SPDM_ALGORITHMS_AEAD_CIPHER_SUITE_AES_256_GCM;
+	spdm_context->local_context.local_cert_chain_provision[0] = data;
+	spdm_context->local_context.local_cert_chain_provision_size[0] =
+		data_size;
+	spdm_context->local_context.slot_count = 1;
+	spdm_reset_message_a(spdm_context);
+	spdm_context->local_context.mut_auth_requested = 0;
+
+	response_size = sizeof(response);
+	spdm_get_response_key_exchange(spdm_context,
+				       m_spdm_key_exchange_request2_size,
+				       &m_spdm_key_exchange_request2,
+				       &response_size, response);
+}
+
+void run_test_harness(IN void *test_buffer, IN uintn test_buffer_size)
+{
+	void *State;
+
+	setup_spdm_test_context(&m_spdm_responder_key_exchange_test_context);
+
+	m_spdm_responder_key_exchange_test_context.test_buffer = test_buffer;
+	m_spdm_responder_key_exchange_test_context.test_buffer_size =
+		test_buffer_size;
+
+	spdm_unit_test_group_setup(&State);
+
+	test_spdm_responder_key_exchange(&State);
+
+	spdm_unit_test_group_teardown(&State);
+}
