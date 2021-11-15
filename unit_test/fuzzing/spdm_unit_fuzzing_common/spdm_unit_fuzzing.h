6eef6289e3461ee16d339b675db427f093ae1903
Xiaohanjlll <hanx.xiao@intel.com>
2021-11-16 02:02:39 +0800

    Add common codes
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

--- unit_test/fuzzing/spdm_unit_fuzzing_common/spdm_unit_fuzzing.h
+++ unit_test/fuzzing/spdm_unit_fuzzing_common/spdm_unit_fuzzing.h
@@ -4,14 +4,15 @@
     License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
 **/
 
-#ifndef __SPDM_UNIT_TEST_H__
-#define __SPDM_UNIT_TEST_H__
+#ifndef __SPDM_UNIT_FUZZING_H__
+#define __SPDM_UNIT_FUZZING_H__
 
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #undef NULL
 #include <base.h>
@@ -20,6 +21,15 @@
 #include <library/spdm_responder_lib.h>
 #include <library/spdm_transport_test_lib.h>
 #include <spdm_common_lib_internal.h>
+
+extern uint8 m_use_measurement_spec;
+extern uint32 m_use_measurement_hash_algo;
+extern uint32 m_use_hash_algo;
+extern uint32 m_use_asym_algo;
+extern uint16 m_use_req_asym_algo;
+extern uint16 m_use_dhe_algo;
+extern uint16 m_use_aead_algo;
+extern uint16 m_use_key_schedule_algo;
 
 #define SPDM_TEST_CONTEXT_SIGNATURE SIGNATURE_32('S', 'T', 'C', 'S')
 
@@ -45,5 +55,8 @@ uintn spdm_unit_test_group_teardown(void **State);
 void setup_spdm_test_context(IN spdm_test_context_t *spdm_test_context);
 
 spdm_test_context_t *get_spdm_test_context(void);
+
+boolean read_input_file(IN char8 *file_name, OUT void **file_data,
+			OUT uintn *file_size);
 
 #endif
