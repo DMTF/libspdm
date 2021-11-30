3ccfcbf43a5405553d2db0dc61270645900ce1d3
Xiaohanjlll <hanx.xiao@intel.com>
2021-12-01 01:03:03 +0800

    Add encap_digests fuzzing
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

---
+++ unit_test/fuzzing/test_spdm_requester_encap_digests/encap_digests.c
@@ -0,0 +1,68 @@
+/**
+    Copyright Notice:
+    Copyright 2021 DMTF. All rights reserved.
+    License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
+**/
+
+#include "spdm_unit_fuzzing.h"
+#include "toolchain_harness.h"
+#include <spdm_device_secret_lib_internal.h>
+#include <internal/libspdm_requester_lib.h>
+
+
+uintn get_max_buffer_size(void)
+{
+	return MAX_SPDM_MESSAGE_BUFFER_SIZE;
+}
+
+void test_spdm_requester_encap_digests(void **State)
+{
+	spdm_test_context_t *spdm_test_context;
+	spdm_context_t *spdm_context;
+	uintn response_size;
+	uint8_t response[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+	uint8_t m_local_certificate_chain[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+
+	spdm_test_context = *State;
+	spdm_context = spdm_test_context->spdm_context;
+	spdm_context->connection_info.connection_state =
+		SPDM_CONNECTION_STATE_NEGOTIATED;
+	spdm_context->local_context.capability.flags |=
+		SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CERT_CAP;
+	spdm_context->connection_info.algorithm.base_hash_algo =
+		m_use_hash_algo;
+	spdm_context->local_context.local_cert_chain_provision[0] =
+		m_local_certificate_chain;
+	spdm_context->local_context.local_cert_chain_provision_size[0] =
+		MAX_SPDM_MESSAGE_BUFFER_SIZE;
+	set_mem(m_local_certificate_chain, MAX_SPDM_MESSAGE_BUFFER_SIZE,
+		(uint8_t)(0xFF));
+	spdm_context->local_context.slot_count = 1;
+
+	response_size = sizeof(response);
+	spdm_get_encap_response_digest(
+		spdm_context, spdm_test_context->test_buffer_size,
+		spdm_test_context->test_buffer, &response_size, response);
+}
+
+spdm_test_context_t m_spdm_requester_encap_digests_test_context = {
+	SPDM_TEST_CONTEXT_SIGNATURE,
+	FALSE,
+};
+
+void run_test_harness(IN void *test_buffer, IN uintn test_buffer_size)
+{
+	void *State;
+
+	setup_spdm_test_context(&m_spdm_requester_encap_digests_test_context);
+
+	m_spdm_requester_encap_digests_test_context.test_buffer = test_buffer;
+	m_spdm_requester_encap_digests_test_context.test_buffer_size =
+		test_buffer_size;
+
+	spdm_unit_test_group_setup(&State);
+
+	test_spdm_requester_encap_digests(&State);
+
+	spdm_unit_test_group_teardown(&State);
+}
