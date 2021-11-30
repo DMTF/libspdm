f34afa768aff50d53b6273cac87328687d469340
Xiaohanjlll <hanx.xiao@intel.com>
2021-12-01 01:25:39 +0800

    Update fuzzing test
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

--- unit_test/fuzzing/test_spdm_responder_capabilities/capabilities.c
+++ unit_test/fuzzing/test_spdm_responder_capabilities/capabilities.c
@@ -13,7 +13,7 @@ uintn get_max_buffer_size(void)
 	return MAX_SPDM_MESSAGE_BUFFER_SIZE;
 }
 
-void test_spdm_responder_capabilities(void **State)
+void test_spdm_responder_capabilities_case1(void **State)
 {
 	spdm_test_context_t *spdm_test_context;
 	spdm_context_t *spdm_context;
@@ -22,6 +22,51 @@ void test_spdm_responder_capabilities(void **State)
 
 	spdm_test_context = *State;
 	spdm_context = spdm_test_context->spdm_context;
+
+	spdm_context->connection_info.connection_state =
+		SPDM_CONNECTION_STATE_AFTER_VERSION;
+
+	response_size = sizeof(response);
+	spdm_get_response_capabilities(spdm_context,
+				       spdm_test_context->test_buffer_size,
+				       spdm_test_context->test_buffer,
+				       &response_size, response);
+}
+
+void test_spdm_responder_capabilities_case2(void **State)
+{
+	spdm_test_context_t *spdm_test_context;
+	spdm_context_t *spdm_context;
+	uintn response_size;
+	uint8_t response[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+
+	spdm_test_context = *State;
+	spdm_context = spdm_test_context->spdm_context;
+
+	spdm_context->response_state = SPDM_RESPONSE_STATE_NORMAL;
+	spdm_context->connection_info.connection_state =
+		SPDM_CONNECTION_STATE_AFTER_VERSION;
+
+	response_size = sizeof(response);
+	spdm_get_response_capabilities(spdm_context,
+				       spdm_test_context->test_buffer_size,
+				       spdm_test_context->test_buffer,
+				       &response_size, response);
+}
+
+void test_spdm_responder_capabilities_case3(void **State)
+{
+	spdm_test_context_t *spdm_test_context;
+	spdm_context_t *spdm_context;
+	uintn response_size;
+	uint8_t response[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+
+	spdm_test_context = *State;
+	spdm_context = spdm_test_context->spdm_context;
+
+	spdm_context->response_state = SPDM_CONNECTION_STATE_AFTER_VERSION;
+	spdm_context->connection_info.connection_state =
+		SPDM_CONNECTION_STATE_AFTER_VERSION;
 
 	response_size = sizeof(response);
 	spdm_get_response_capabilities(spdm_context,
@@ -41,12 +86,13 @@ void run_test_harness(IN void *test_buffer, IN uintn test_buffer_size)
 	setup_spdm_test_context(&test_spdm_responder_context);
 
 	test_spdm_responder_context.test_buffer = test_buffer;
-	test_spdm_responder_context.test_buffer_size =
-		test_buffer_size;
+	test_spdm_responder_context.test_buffer_size = test_buffer_size;
 
 	spdm_unit_test_group_setup(&State);
 
-	test_spdm_responder_capabilities(&State);
+	test_spdm_responder_capabilities_case1(&State);
+	test_spdm_responder_capabilities_case2(&State);
+	test_spdm_responder_capabilities_case3(&State);
 
 	spdm_unit_test_group_teardown(&State);
 }
