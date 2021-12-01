be8e3df28f5d7b8aa1625b87296592aa106337ee
Xiaohanjlll <hanx.xiao@intel.com>
2021-12-02 01:10:15 +0800

    Repair fuzzy test case tool(byte alignment)
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

--- unit_test/fuzzing/spdm_unit_fuzzing_common/common.c
+++ unit_test/fuzzing/spdm_unit_fuzzing_common/common.c
@@ -52,3 +52,39 @@ uintn spdm_unit_test_group_teardown(void **State)
 	spdm_test_context->spdm_context = NULL;
 	return 0;
 }
+
+boolean read_input_file(IN char8 *file_name, OUT void **file_data,
+			OUT uintn *file_size)
+{
+	FILE *fp_in;
+	uintn temp_result;
+
+	if ((fp_in = fopen(file_name, "rb")) == NULL) {
+		printf("Unable to open file %s\n", file_name);
+		*file_data = NULL;
+		return FALSE;
+	}
+
+	fseek(fp_in, 0, SEEK_END);
+	*file_size = ftell(fp_in);
+
+	*file_data = (void *)malloc(*file_size);
+	if (NULL == *file_data) {
+		printf("No sufficient memory to allocate %s\n", file_name);
+		fclose(fp_in);
+		return FALSE;
+	}
+
+	fseek(fp_in, 0, SEEK_SET);
+	temp_result = fread(*file_data, 1, *file_size, fp_in);
+	if (temp_result != *file_size) {
+		printf("Read input file error %s", file_name);
+		free((void *)*file_data);
+		fclose(fp_in);
+		return FALSE;
+	}
+
+	fclose(fp_in);
+
+	return TRUE;
+}
