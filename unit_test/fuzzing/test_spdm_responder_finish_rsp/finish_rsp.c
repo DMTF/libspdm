6ba89468e7fa8a909c70205a6e65571c7cc6a25d
Xiaohanjlll <hanx.xiao@intel.com>
2021-12-09 17:12:56 +0800

    Update finish_rsp fuzzing test case
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

--- unit_test/fuzzing/test_spdm_responder_finish_rsp/finish_rsp.c
+++ unit_test/fuzzing/test_spdm_responder_finish_rsp/finish_rsp.c
@@ -6,8 +6,8 @@
 
 #include "spdm_unit_fuzzing.h"
 #include "toolchain_harness.h"
-#include "internal/libspdm_responder_lib.h"
-#include "spdm_device_secret_lib_internal.h"
+#include <internal/libspdm_responder_lib.h>
+#include <spdm_device_secret_lib_internal.h>
 
 uintn get_max_buffer_size(void)
 {
@@ -52,6 +52,10 @@ void test_spdm_responder_finish_case1(void **State)
         SPDM_GET_CAPABILITIES_REQUEST_FLAGS_KEY_EX_CAP;
     spdm_context->local_context.capability.flags |=
         SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_KEY_EX_CAP;
+    spdm_context->connection_info.capability.flags |=
+        SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_HANDSHAKE_IN_THE_CLEAR_CAP;
+    spdm_context->local_context.capability.flags |=
+        SPDM_GET_CAPABILITIES_REQUEST_FLAGS_HANDSHAKE_IN_THE_CLEAR_CAP;
     spdm_context->connection_info.algorithm.base_hash_algo =
         m_use_hash_algo;
     spdm_context->connection_info.algorithm.base_asym_algo =
@@ -92,16 +96,95 @@ void test_spdm_responder_finish_case1(void **State)
         session_info->secured_message_context,
         SPDM_SESSION_STATE_HANDSHAKING);
 
-    spdm_context->connection_info.capability.flags |=
-        SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_HANDSHAKE_IN_THE_CLEAR_CAP;
-    spdm_context->local_context.capability.flags |=
-        SPDM_GET_CAPABILITIES_REQUEST_FLAGS_HANDSHAKE_IN_THE_CLEAR_CAP;
-
     response_size = sizeof(response);
     spdm_get_response_finish(spdm_context,
                  spdm_test_context->test_buffer_size,
                  spdm_test_context->test_buffer, &response_size,
                  response);
+}
+
+void test_spdm_responder_finish_case2(void **state)
+{
+	spdm_test_context_t *spdm_test_context;
+	spdm_context_t *spdm_context;
+	uintn response_size;
+	uint8_t response[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+	void *data1;
+	uintn data_size1;
+	void *data2;
+	uintn data_size2;
+	spdm_session_info_t *session_info;
+	uint32_t session_id;
+	uint32_t hash_size;
+	uint8_t m_dummy_buffer[MAX_HASH_SIZE];
+
+	spdm_test_context = *state;
+	spdm_context = spdm_test_context->spdm_context;
+	spdm_context->connection_info.connection_state =
+		SPDM_CONNECTION_STATE_NEGOTIATED;
+	spdm_context->connection_info.capability.flags |=
+		SPDM_GET_CAPABILITIES_REQUEST_FLAGS_KEY_EX_CAP;
+    spdm_context->local_context.capability.flags |=
+        SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_KEY_EX_CAP;
+    spdm_context->connection_info.capability.flags |=
+		SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_HANDSHAKE_IN_THE_CLEAR_CAP;
+	spdm_context->local_context.capability.flags |=
+		SPDM_GET_CAPABILITIES_REQUEST_FLAGS_HANDSHAKE_IN_THE_CLEAR_CAP;
+	spdm_context->connection_info.algorithm.base_hash_algo =
+		m_use_hash_algo;
+	spdm_context->connection_info.algorithm.base_asym_algo =
+		m_use_asym_algo;
+	spdm_context->connection_info.algorithm.req_base_asym_alg =
+		m_use_req_asym_algo;
+	spdm_context->connection_info.algorithm.measurement_spec =
+		m_use_measurement_spec;
+	spdm_context->connection_info.algorithm.measurement_hash_algo =
+		m_use_measurement_hash_algo;
+	spdm_context->connection_info.algorithm.dhe_named_group =
+		m_use_dhe_algo;
+	spdm_context->connection_info.algorithm.aead_cipher_suite =
+		m_use_aead_algo;
+	read_responder_public_certificate_chain(m_use_hash_algo,
+						m_use_asym_algo, &data1,
+						&data_size1, NULL, NULL);
+	spdm_context->local_context.local_cert_chain_provision[0] = data1;
+	spdm_context->local_context.local_cert_chain_provision_size[0] =
+		data_size1;
+	spdm_context->connection_info.local_used_cert_chain_buffer = data1;
+	spdm_context->connection_info.local_used_cert_chain_buffer_size =
+		data_size1;
+	spdm_context->local_context.slot_count = 1;
+	libspdm_reset_message_a(spdm_context);
+	spdm_context->local_context.mut_auth_requested = 1;
+	read_requester_public_certificate_chain(m_use_hash_algo,
+						m_use_req_asym_algo, &data2,
+						&data_size2, NULL, NULL);
+	spdm_context->local_context.peer_cert_chain_provision = data2;
+	spdm_context->local_context.peer_cert_chain_provision_size = data_size2;
+	copy_mem(spdm_context->connection_info.peer_used_cert_chain_buffer,
+		 data2, data_size2);
+	spdm_context->connection_info.peer_used_cert_chain_buffer_size =
+		data_size2;
+
+	session_id = 0xFFFFFFFF;
+	spdm_context->latest_session_id = session_id;
+	session_info = &spdm_context->session_info[0];
+	spdm_session_info_init(spdm_context, session_info, session_id, FALSE);
+	hash_size = spdm_get_hash_size(m_use_hash_algo);
+	set_mem(m_dummy_buffer, hash_size, (uint8_t)(0xFF));
+	spdm_secured_message_set_request_finished_key(
+		session_info->secured_message_context, m_dummy_buffer,
+		hash_size);
+	spdm_secured_message_set_session_state(
+		session_info->secured_message_context,
+		SPDM_SESSION_STATE_HANDSHAKING);
+	session_info->mut_auth_requested = 1;
+
+	response_size = sizeof(response);
+	spdm_get_response_finish(spdm_context,
+				 spdm_test_context->test_buffer_size,
+				 spdm_test_context->test_buffer, &response_size,
+				 response);
 }
 
 void run_test_harness(IN void *test_buffer, IN uintn test_buffer_size)
@@ -117,6 +200,7 @@ void run_test_harness(IN void *test_buffer, IN uintn test_buffer_size)
     spdm_unit_test_group_setup(&State);
 
     test_spdm_responder_finish_case1(&State);
+    test_spdm_responder_finish_case2(&State);
 
     spdm_unit_test_group_teardown(&State);
 }
