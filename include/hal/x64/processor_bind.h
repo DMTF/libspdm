fcd5595b8c84d48a2205faa696c45e6a7ad3df1c
Steven Bellock <sbellock@nvidia.com>
2021-12-14 08:39:18 -0800

    Fixed OpenSSL typos and added ultimate intent for OpenSSL and libspdm
    Signed-off-by: Steven Bellock <sbellock@nvidia.com>

--- include/hal/x64/processor_bind.h
+++ include/hal/x64/processor_bind.h
@@ -100,25 +100,28 @@
 
 // LIBSPDM_OPENSSL_STDINT_WORKAROUND
 //
-// This is a workaround for OpenSLL compilation problems when used with <stdint.h>
+// This is a workaround for OpenSSL compilation problems when used with <stdint.h>
 // on Windows platforms built with Visual Studio. Including <stdint.h> pulls in
 // <vcruntime.h>, which causes the type size_t to be defined. The size_t type
 // depends on if _WIN32 or _WIN64 is defined. The default if neither is defined
 // is the 32-bit version of size_t.
 //
-// Our Openssl compilation requires _WIN32 and _WIN64 to NOT be defined.
+// Our OpenSSL compilation requires _WIN32 and _WIN64 to NOT be defined.
 // This will force the <vcruntime.h> to use the wrong 32-bit definition of size_t
 // if we are compiling as 64-bit. This 32-bit definition then does not agree with
 // the 64-bit definition defined in libspdm and generates compile errors.
 //
-// To workaround this issue, I created a LIBSPDM_OPENSSL_STDINT_WORKAROUND define
-// that is only defined for compilation via tha makefile of the openssl library
+// To workaround this issue, LIBSPDM_OPENSSL_STDINT_WORKAROUND was created
+// that is only defined for compilation via tha makefile of the OpenSSL library
 // portion of libspdm.
 //
-// This will lead to _WIN32 and _WIN64 to be NOT defined when reaching the openssl
+// This will lead to _WIN32 and _WIN64 to be NOT defined when reaching the OpenSSL
 // portions of a compilation unit (header files + c file), thus meeting the
-// no Win32/Win64 requirement for openssl, but will still be defined when compiling
+// no Win32/Win64 requirement for OpenSSL, but will still be defined when compiling
 // the <vcruntime.h> file in the compilation unit (and getting the right size_t).
+//
+// In the future libspdm intends to use the Windows native compilation flags and defines,
+// in place of the UEFI profile / personality.
 
 #ifdef LIBSPDM_OPENSSL_STDINT_WORKAROUND
 #undef _WIN32
