4f2d639fda14cbb86ba8a840032362ad4ae85e07
Jiewen Yao <jiewen.yao@intel.com>
2022-01-04 09:42:47 +0800

    Fix typo in PLDM definition and add more.
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- include/industry_standard/pldm.h
+++ include/industry_standard/pldm.h
@@ -17,6 +17,16 @@
 
 #pragma pack(1)
 
+/* PLDM Header first byte*/
+#define PLDM_HEADER_REQUEST_MASK 0x80
+#define PLDM_HEADER_DATAGRAM_MASK 0x40
+#define PLDM_HEADER_INSTANCE_ID_MASK 0x1F
+
+/* PLDM Header second byte*/
+#define PLDM_HEADER_VERSION 0x00
+#define PLDM_HEADER_VERSION_MASK 0xC0
+#define PLDM_HEADER_TYPE_MASK 0x3F
+
 typedef struct {
     uint8_t instance_id;
     uint8_t pldm_type;
@@ -32,13 +42,13 @@ typedef struct {
 #define PLDM_BASE_CODE_ERROR 1
 
 #define PLDM_MESSAGE_TYPE_CONTROL_DISCOVERY 0x00
-#define MCTP_MESSAGE_TYPE_SMBIOS 0x01
-#define MCTP_MESSAGE_TYPE_PLATFORM_MONITORING_CONTROL 0x02
-#define MCTP_MESSAGE_TYPE_BIOS_CONTROL_CONFIGURATION 0x03
-#define MCTP_MESSAGE_TYPE_FRU_DATA 0x04
-#define MCTP_MESSAGE_TYPE_FIRMWARE_UPDATE 0x05
-#define MCTP_MESSAGE_TYPE_REDFISH_DEVICE_ENABLEMENT 0x06
-#define MCTP_MESSAGE_TYPE_OEM 0x3F
+#define PLDM_MESSAGE_TYPE_SMBIOS 0x01
+#define PLDM_MESSAGE_TYPE_PLATFORM_MONITORING_CONTROL 0x02
+#define PLDM_MESSAGE_TYPE_BIOS_CONTROL_CONFIGURATION 0x03
+#define PLDM_MESSAGE_TYPE_FRU_DATA 0x04
+#define PLDM_MESSAGE_TYPE_FIRMWARE_UPDATE 0x05
+#define PLDM_MESSAGE_TYPE_REDFISH_DEVICE_ENABLEMENT 0x06
+#define PLDM_MESSAGE_TYPE_OEM 0x3F
 
 
 /* PLDM_MESSAGE_TYPE_CONTROL_DISCOVERY*/
@@ -48,6 +58,20 @@ typedef struct {
 #define PLDM_CONTROL_DISCOVERY_COMMAND_GET_PLDM_VERSION 0x03
 #define PLDM_CONTROL_DISCOVERY_COMMAND_GET_PLDM_TYPES 0x04
 #define PLDM_CONTROL_DISCOVERY_COMMAND_GET_PLDM_COMMANDS 0x05
+
+/* PLDM GET_TID request*/
+
+typedef struct {
+    pldm_message_header_t pldm_header;
+} pldm_get_tid_request_t;
+
+/* PLDM GET_TID response*/
+
+typedef struct {
+    pldm_message_header_t pldm_header;
+    pldm_message_response_header_t pldm_response_header;
+    uint8_t tid;
+} pldm_get_tid_response_t;
 
 #pragma pack()
 
