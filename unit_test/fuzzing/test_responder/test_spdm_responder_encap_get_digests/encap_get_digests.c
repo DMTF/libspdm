d162b0a44e21241c6440f87e21717ee9c3f115ab
Xiaohanjlll <hanx.xiao@intel.com>
2022-01-06 00:22:55 +0800

    update encap_get_digests fuzzing
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

--- unit_test/fuzzing/test_responder/test_spdm_responder_encap_get_digests/encap_get_digests.c
+++ unit_test/fuzzing/test_responder/test_spdm_responder_encap_get_digests/encap_get_digests.c
@@ -4,11 +4,10 @@
     License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
 **/
 
+#include "internal/libspdm_responder_lib.h"
+#include "spdm_device_secret_lib_internal.h"
 #include "spdm_unit_fuzzing.h"
 #include "toolchain_harness.h"
-#include "spdm_device_secret_lib_internal.h"
-#include "internal/libspdm_responder_lib.h"
-
 
 uintn get_max_buffer_size(void)
 {
@@ -20,7 +19,7 @@ spdm_test_context_t m_spdm_responder_encap_get_digests_test_context = {
     FALSE,
 };
 
-void test_spdm_responder_encap_get_digests(void **State)
+void test_spdm_responder_encap_get_digests_case1(void **State)
 {
     spdm_test_context_t *spdm_test_context;
     spdm_context_t *spdm_context;
@@ -29,24 +28,52 @@ void test_spdm_responder_encap_get_digests(void **State)
 
     spdm_test_context = *State;
     spdm_context = spdm_test_context->spdm_context;
-    spdm_context->connection_info.connection_state =
-        LIBSPDM_CONNECTION_STATE_NEGOTIATED;
-    spdm_context->local_context.capability.flags |=
-        SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CERT_CAP;
-    spdm_context->connection_info.algorithm.base_hash_algo =
-        m_use_hash_algo;
-    spdm_context->local_context.local_cert_chain_provision[0] =
-        m_local_certificate_chain;
+    spdm_context->connection_info.connection_state = LIBSPDM_CONNECTION_STATE_NEGOTIATED;
+    spdm_context->local_context.capability.flags |= SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CERT_CAP;
+    spdm_context->connection_info.algorithm.base_hash_algo = m_use_hash_algo;
+    spdm_context->local_context.local_cert_chain_provision[0] = m_local_certificate_chain;
     spdm_context->local_context.local_cert_chain_provision_size[0] =
         LIBSPDM_MAX_MESSAGE_BUFFER_SIZE;
-    set_mem(m_local_certificate_chain, LIBSPDM_MAX_MESSAGE_BUFFER_SIZE,
-        (uint8_t)(0xFF));
+    set_mem(m_local_certificate_chain, LIBSPDM_MAX_MESSAGE_BUFFER_SIZE, (uint8_t)(0xFF));
     spdm_context->local_context.slot_count = 1;
 
-    need_continue = FALSE;
-    spdm_process_encap_response_digest(
-        spdm_context, spdm_test_context->test_buffer_size,
-        spdm_test_context->test_buffer, &need_continue);
+    spdm_process_encap_response_digest(spdm_context, spdm_test_context->test_buffer_size,
+                                       spdm_test_context->test_buffer, &need_continue);
+}
+
+void test_spdm_get_encap_request_get_digest_case2(void **State)
+{
+    spdm_test_context_t *spdm_test_context;
+    spdm_get_digest_request_t *spdm_request;
+    spdm_context_t *spdm_context;
+    uintn encap_request_size;
+    void *data;
+    uintn data_size;
+
+    spdm_test_context = *State;
+    spdm_context = spdm_test_context->spdm_context;
+    encap_request_size = spdm_test_context->test_buffer_size;
+
+    if (encap_request_size < sizeof(spdm_get_digest_request_t)) {
+        encap_request_size = sizeof(spdm_get_digest_request_t);
+    }
+    spdm_request = malloc(encap_request_size);
+
+    spdm_context->connection_info.connection_state = LIBSPDM_CONNECTION_STATE_NEGOTIATED;
+    spdm_context->connection_info.capability.flags = 0;
+    spdm_context->connection_info.capability.flags |= SPDM_GET_CAPABILITIES_REQUEST_FLAGS_CERT_CAP;
+    spdm_context->connection_info.version.major_version = 1;
+    spdm_context->connection_info.version.minor_version = 1;
+    read_responder_public_certificate_chain(m_use_hash_algo, m_use_asym_algo, &data, &data_size,
+                                            NULL, NULL);
+    spdm_context->local_context.local_cert_chain_provision_size[0] = data_size;
+    spdm_context->local_context.local_cert_chain_provision[0] = data;
+    spdm_context->connection_info.algorithm.base_asym_algo = m_use_asym_algo;
+    spdm_context->connection_info.algorithm.base_hash_algo = m_use_hash_algo;
+    libspdm_reset_message_b(spdm_context);
+
+    spdm_get_encap_request_get_digest(spdm_context, &encap_request_size, spdm_request);
+    free(spdm_request);
 }
 
 void run_test_harness(IN void *test_buffer, IN uintn test_buffer_size)
@@ -56,12 +83,15 @@ void run_test_harness(IN void *test_buffer, IN uintn test_buffer_size)
     setup_spdm_test_context(&m_spdm_responder_encap_get_digests_test_context);
 
     m_spdm_responder_encap_get_digests_test_context.test_buffer = test_buffer;
-    m_spdm_responder_encap_get_digests_test_context.test_buffer_size =
-        test_buffer_size;
+    m_spdm_responder_encap_get_digests_test_context.test_buffer_size = test_buffer_size;
 
+    /* Success Case */
     spdm_unit_test_group_setup(&State);
+    test_spdm_responder_encap_get_digests_case1(&State);
+    spdm_unit_test_group_teardown(&State);
 
-    test_spdm_responder_encap_get_digests(&State);
-
+    /* Success Case */
+    spdm_unit_test_group_setup(&State);
+    test_spdm_get_encap_request_get_digest_case2(&State);
     spdm_unit_test_group_teardown(&State);
 }
