0225f868de19bdb108079a3793a7c97afc2ac632
Xiaohanjlll <hanx.xiao@intel.com>
2022-01-27 19:20:33 +0800

    Add common functions
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

--- unit_test/fuzzing/spdm_unit_fuzzing_common/toolchain_harness.h
+++ unit_test/fuzzing/spdm_unit_fuzzing_common/toolchain_harness.h
@@ -14,6 +14,8 @@
 
 #define TEST_ALIGNMENT 4
 #define TEST_MESSAGE_TYPE_SPDM 0x01
+#define TEST_MESSAGE_TYPE_SECURED_TEST 0x02
+#define LIBSPDM_MAX_BUFFER_SIZE 64
 #define NULL ((void *)0)
 
 void run_test_harness(IN void *test_buffer, IN uintn test_buffer_size);
