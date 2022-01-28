dc53092c91f1a7c88f64d37d3fa588f1bfffa3db
Abe Kohandel <abe.kohandel@intel.com>
2022-01-27 18:39:51 -0800

    Add Xtensa architecture support
    This changes introduces support for Cadence's Xtensa architecture
    
    Signed-off-by: Abe Kohandel <abe.kohandel@intel.com>

---
+++ include/hal/xtensa/processor_bind.h
@@ -0,0 +1,34 @@
+/**
+ *  Copyright Notice:
+ *  Copyright 2021-2022 DMTF. All rights reserved.
+ *  License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
+ **/
+
+#ifndef __PROCESSOR_BIND_H__
+#define __PROCESSOR_BIND_H__
+
+
+/* Define the processor type so other code can make processor based choices*/
+
+#define LIBSPDM_CPU_XTENSA
+
+
+#ifndef LIBSPDM_STDINT_ALT
+#include <stdint.h>
+#else
+#include LIBSPDM_STDINT_ALT
+#endif
+
+#ifndef LIBSPDM_STDBOOL_ALT
+#include <stdbool.h>
+#else
+#include LIBSPDM_STDBOOL_ALT
+#endif
+
+#ifndef LIBSPDM_STDDEF_ALT
+#include <stddef.h>
+#else
+#include LIBSPDM_STDDEF_ALT
+#endif
+
+#endif
