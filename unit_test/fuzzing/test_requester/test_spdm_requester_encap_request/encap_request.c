e0b683d4c09b1ec6750c2d636412ac65eef87a17
Kong, Richard <richard.kong@intel.com>
2022-02-15 09:15:49 -0800

    Use copy_mem_s in unit_tests\fuzzing (common/toolchain_harness, requester:(challenge, encap_key_update, encap_request)).
    Signed-off-by: Kong, Richard <richard.kong@intel.com>

--- unit_test/fuzzing/test_requester/test_spdm_requester_encap_request/encap_request.c
+++ unit_test/fuzzing/test_requester/test_spdm_requester_encap_request/encap_request.c
@@ -48,27 +48,30 @@ return_status spdm_device_receive_message(IN void *spdm_context, IN OUT uintn *r
         temp_buf_size = 16;
         test_message_size = 16;
 
-        copy_mem((uint8_t *)temp_buf,
-                 (uint8_t *)spdm_test_context->test_buffer + test_message_header_size +
-                 test_message_size * sub_index,
-                 temp_buf_size);
+        copy_mem_s((uint8_t *)temp_buf,
+                   sizeof(temp_buf),
+                   (uint8_t *)spdm_test_context->test_buffer + test_message_header_size +
+                   test_message_size * sub_index,
+                   temp_buf_size);
         break;
     case 5:
         temp_buf_size = 44;
         test_message_size = 16;
 
-        copy_mem((uint8_t *)temp_buf,
-                 (uint8_t *)spdm_test_context->test_buffer + test_message_header_size +
-                 temp_buf_size * sub_index,
-                 temp_buf_size);
+        copy_mem_s((uint8_t *)temp_buf,
+                   sizeof(temp_buf),
+                   (uint8_t *)spdm_test_context->test_buffer + test_message_header_size +
+                   temp_buf_size * sub_index,
+                   temp_buf_size);
         break;
     case 6:
         temp_buf_size = 8;
         test_message_size = 16;
-        copy_mem((uint8_t *)temp_buf,
-                 (uint8_t *)spdm_test_context->test_buffer + test_message_header_size +
-                 test_message_size * sub_index + 28,
-                 temp_buf_size);
+        copy_mem_s((uint8_t *)temp_buf,
+                   sizeof(temp_buf),
+                   (uint8_t *)spdm_test_context->test_buffer + test_message_header_size +
+                   test_message_size * sub_index + 28,
+                   temp_buf_size);
         sub_index = 0;
         break;
     }
@@ -130,8 +133,9 @@ void test_spdm_requester_encap_request(void **State)
 #if LIBSPDM_RECORD_TRANSCRIPT_DATA_SUPPORT
     spdm_context->connection_info.peer_used_cert_chain_buffer_size =
         data_size;
-    copy_mem(spdm_context->connection_info.peer_used_cert_chain_buffer,
-             data, data_size);
+    copy_mem_s(spdm_context->connection_info.peer_used_cert_chain_buffer,
+               sizeof(spdm_context->connection_info.peer_used_cert_chain_buffer),
+               data, data_size);
 #endif
 
     session_id = 0xFFFFFFFF;
