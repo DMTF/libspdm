9eb01e5c591a9142cbd2c98f3088469ba68acdff
Kong, Richard <richard.kong@intel.com>
2022-02-16 09:55:44 -0800

    Fix style compliance failures (spacing).
    Signed-off-by: Kong, Richard <richard.kong@intel.com>

--- unit_test/test_spdm_requester/heartbeat.c
+++ unit_test/test_spdm_requester/heartbeat.c
@@ -1448,7 +1448,7 @@ void test_spdm_requester_heartbeat_case11(void **state)
 #endif
     zero_mem(m_local_psk_hint, 32);
      copy_mem_s(&m_local_psk_hint[0], sizeof(m_local_psk_hint),
-               TEST_PSK_HINT_STRING, sizeof(TEST_PSK_HINT_STRING));
+                TEST_PSK_HINT_STRING, sizeof(TEST_PSK_HINT_STRING));
     spdm_context->local_context.psk_hint_size =
         sizeof(TEST_PSK_HINT_STRING);
     spdm_context->local_context.psk_hint = m_local_psk_hint;
