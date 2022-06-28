53c28eef93a0aee81eb66a693c0b617307eb364c
Steven Bellock <sbellock@nvidia.com>
2022-06-28 16:39:20 -0700

    Check DataTransferSize and MaxSPDMmsgSize
    Fixes #947
    
    Signed-off-by: Steven Bellock <sbellock@nvidia.com>

--- unit_test/test_spdm_responder/capabilities.c
+++ unit_test/test_spdm_responder/capabilities.c
@@ -439,6 +439,20 @@ spdm_get_capabilities_request_t m_libspdm_get_capabilities_request19 = {
 };
 size_t m_libspdm_get_capabilities_request19_size = sizeof(m_libspdm_get_capabilities_request19);
 
+spdm_get_capabilities_request_t m_libspdm_get_capabilities_request25 = {
+    {
+        SPDM_MESSAGE_VERSION_12,
+        SPDM_GET_CAPABILITIES,
+    }, /*header*/
+    0x00, /*reserved*/
+    0x01, /*ct_exponent*/
+    0x0000, /*reserved, 2 bytes*/
+    0,
+    LIBSPDM_MAX_MESSAGE_BUFFER_SIZE - 1,
+    LIBSPDM_MAX_MESSAGE_BUFFER_SIZE,
+};
+size_t m_libspdm_get_capabilities_request25_size = sizeof(m_libspdm_get_capabilities_request19);
+
 void libspdm_test_responder_capabilities_case1(void **state)
 {
     libspdm_return_t status;
@@ -1179,6 +1193,37 @@ void libspdm_test_responder_capabilities_case24(void **state)
     assert_int_equal(spdm_response->max_spdm_msg_size, LIBSPDM_MAX_MESSAGE_BUFFER_SIZE);
 }
 
+void libspdm_test_responder_capabilities_case25(void **state)
+{
+    libspdm_return_t status;
+    libspdm_test_context_t *spdm_test_context;
+    libspdm_context_t *spdm_context;
+    size_t response_size;
+    uint8_t response[LIBSPDM_MAX_MESSAGE_BUFFER_SIZE];
+    spdm_capabilities_response_t *spdm_response;
+
+    spdm_test_context = *state;
+    spdm_context = spdm_test_context->spdm_context;
+    spdm_test_context->case_id = 0x19;
+    spdm_context->connection_info.connection_state =
+        LIBSPDM_CONNECTION_STATE_AFTER_VERSION;
+
+    response_size = sizeof(response);
+    status = libspdm_get_response_capabilities(
+        spdm_context, m_libspdm_get_capabilities_request25_size,
+        &m_libspdm_get_capabilities_request25, &response_size, response);
+    assert_int_equal(status, LIBSPDM_STATUS_SUCCESS);
+    assert_int_equal(response_size, sizeof(spdm_error_response_t));
+    spdm_response = (void *)response;
+    assert_int_equal(m_libspdm_get_capabilities_request25.header.spdm_version,
+                     spdm_response->header.spdm_version);
+    assert_int_equal(spdm_response->header.request_response_code,
+                     SPDM_ERROR);
+    assert_int_equal(spdm_response->header.param1,
+                     SPDM_ERROR_CODE_INVALID_REQUEST);
+    assert_int_equal(spdm_response->header.param2, 0);
+}
+
 libspdm_test_context_t m_libspdm_responder_capabilities_test_context = {
     LIBSPDM_TEST_CONTEXT_VERSION,
     false,
@@ -1233,6 +1278,8 @@ int libspdm_responder_capabilities_test_main(void)
         cmocka_unit_test(libspdm_test_responder_capabilities_case23),
         /* V1.2 Success case, all possible flags set*/
         cmocka_unit_test(libspdm_test_responder_capabilities_case24),
+        /* CHUNK_CAP == 0 and data_transfer_size != max_spdm_msg_size should result in error. */
+        cmocka_unit_test(libspdm_test_responder_capabilities_case25),
     };
 
     libspdm_setup_test_context(&m_libspdm_responder_capabilities_test_context);
