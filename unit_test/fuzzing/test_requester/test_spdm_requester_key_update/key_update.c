e970a503986a8c4856057ce2ac2aa157ad3ba233
Xiaohanjlll <hanx.xiao@intel.com>
2022-07-27 18:51:44 +0800

    Fix fuzzy test cases
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

--- unit_test/fuzzing/test_requester/test_spdm_requester_key_update/key_update.c
+++ unit_test/fuzzing/test_requester/test_spdm_requester_key_update/key_update.c
@@ -119,10 +119,12 @@ libspdm_return_t libspdm_device_receive_message(void *spdm_context, size_t *resp
     libspdm_test_context_t *spdm_test_context;
     uint8_t *spdm_response;
     size_t spdm_response_size;
-    uint8_t temp_buf[LIBSPDM_MAX_MESSAGE_BUFFER_SIZE];
     size_t test_message_header_size;
     uint32_t session_id;
     libspdm_session_info_t *session_info;
+    uint8_t *scratch_buffer;
+    size_t scratch_buffer_size;
+    size_t aead_tag_max_size;
     static uint8_t sub_index = 0;
 
     session_id = 0xFFFFFFFF;
@@ -133,15 +135,27 @@ libspdm_return_t libspdm_device_receive_message(void *spdm_context, size_t *resp
 
     spdm_test_context = libspdm_get_test_context();
     test_message_header_size = libspdm_transport_test_get_header_size(spdm_context);
-    spdm_response = (void *)((uint8_t *)temp_buf + test_message_header_size);
     spdm_response_size = spdm_test_context->test_buffer_size;
-    if (spdm_response_size > sizeof(temp_buf) - test_message_header_size - LIBSPDM_TEST_ALIGNMENT) {
-        spdm_response_size = sizeof(temp_buf) - test_message_header_size - LIBSPDM_TEST_ALIGNMENT;
+    /* limit the encoding buffer to avoid assert, because the input buffer is controlled by the the libspdm consumer. */
+    test_message_header_size += sizeof(spdm_secured_message_a_data_header1_t) +
+                                2 +     /* MCTP_SEQUENCE_NUMBER_COUNT */
+                                sizeof(spdm_secured_message_a_data_header2_t) +
+                                sizeof(spdm_secured_message_cipher_header_t) +
+                                32 /* MCTP_MAX_RANDOM_NUMBER_COUNT */;
+    aead_tag_max_size = LIBSPDM_MAX_AEAD_TAG_SIZE;
+
+    /* For secure message, message is in sender buffer, we need copy it to scratch buffer.
+     * transport_message is always in sender buffer. */
+    libspdm_get_scratch_buffer(spdm_context, (void **)&scratch_buffer, &scratch_buffer_size);
+    spdm_response = (void *)(scratch_buffer + test_message_header_size);
+    spdm_response_size = spdm_test_context->test_buffer_size;
+    if (spdm_response_size >
+        LIBSPDM_MAX_MESSAGE_BUFFER_SIZE - test_message_header_size - aead_tag_max_size -
+        LIBSPDM_TEST_ALIGNMENT) {
+        spdm_response_size = LIBSPDM_MAX_MESSAGE_BUFFER_SIZE - test_message_header_size -
+                             aead_tag_max_size -
+                             LIBSPDM_TEST_ALIGNMENT;
     }
-    libspdm_copy_mem((uint8_t *)temp_buf + test_message_header_size,
-                     sizeof(temp_buf) - test_message_header_size,
-                     (uint8_t *)spdm_test_context->test_buffer,
-                     spdm_response_size);
 
     if (spdm_response_size > (sub_index + 1) * sizeof(spdm_key_update_response_t)) {
         spdm_response_size = sizeof(spdm_key_update_response_t);
@@ -150,11 +164,12 @@ libspdm_return_t libspdm_device_receive_message(void *spdm_context, size_t *resp
     } else {
         return LIBSPDM_STATUS_RECEIVE_FAIL;
     }
-    libspdm_copy_mem((uint8_t *)temp_buf + test_message_header_size,
-                     sizeof(temp_buf) - test_message_header_size,
-                     (uint8_t *)spdm_test_context->test_buffer +
-                     sizeof(spdm_key_update_response_t) * sub_index,
-                     spdm_response_size);
+
+    libspdm_copy_mem (scratch_buffer + test_message_header_size,
+                      scratch_buffer_size - test_message_header_size,
+                      (uint8_t *)spdm_test_context->test_buffer +
+                      sizeof(spdm_key_update_response_t) * sub_index,
+                      spdm_response_size);
 
     libspdm_transport_test_encode_message(spdm_context, &session_id, false, false,
                                           spdm_response_size,
--- unit_test/fuzzing/test_requester/test_spdm_requester_psk_finish/psk_finish.c
+++ unit_test/fuzzing/test_requester/test_spdm_requester_psk_finish/psk_finish.c
@@ -66,21 +66,39 @@ libspdm_return_t libspdm_device_receive_message(void *spdm_context, size_t *resp
     libspdm_test_context_t *spdm_test_context;
     uint8_t *spdm_response;
     size_t spdm_response_size;
-    uint8_t temp_buf[LIBSPDM_MAX_MESSAGE_BUFFER_SIZE];
     size_t test_message_header_size;
     uint32_t session_id;
     libspdm_session_info_t *session_info;
+    size_t aead_tag_max_size;
+    uint8_t *scratch_buffer;
+    size_t scratch_buffer_size;
 
     session_id = 0xFFFFFFFF;
     spdm_test_context = libspdm_get_test_context();
     test_message_header_size = libspdm_transport_test_get_header_size(spdm_context);
-    spdm_response = (void *)((uint8_t *)temp_buf + test_message_header_size);
+    /* limit the encoding buffer to avoid assert, because the input buffer is controlled by the the libspdm consumer. */
+    test_message_header_size += sizeof(spdm_secured_message_a_data_header1_t) +
+                                2 + /* MCTP_SEQUENCE_NUMBER_COUNT */
+                                sizeof(spdm_secured_message_a_data_header2_t) +
+                                sizeof(spdm_secured_message_cipher_header_t) +
+                                32; /* MCTP_MAX_RANDOM_NUMBER_COUNT */
+    aead_tag_max_size = LIBSPDM_MAX_AEAD_TAG_SIZE;
+
+    /* For secure message, message is in sender buffer, we need copy it to scratch buffer.
+     * transport_message is always in sender buffer. */
+    libspdm_get_scratch_buffer(spdm_context, (void **)&scratch_buffer, &scratch_buffer_size);
+    spdm_response = (void *)(scratch_buffer + test_message_header_size);
     spdm_response_size = spdm_test_context->test_buffer_size;
-    if (spdm_response_size > sizeof(temp_buf) - test_message_header_size - LIBSPDM_TEST_ALIGNMENT) {
-        spdm_response_size = sizeof(temp_buf) - test_message_header_size - LIBSPDM_TEST_ALIGNMENT;
+    if (spdm_response_size >
+        LIBSPDM_MAX_MESSAGE_BUFFER_SIZE - test_message_header_size - aead_tag_max_size -
+        LIBSPDM_TEST_ALIGNMENT) {
+        spdm_response_size = LIBSPDM_MAX_MESSAGE_BUFFER_SIZE - test_message_header_size -
+                             aead_tag_max_size -
+                             LIBSPDM_TEST_ALIGNMENT;
     }
-    libspdm_copy_mem((uint8_t *)temp_buf + test_message_header_size,
-                     sizeof(temp_buf) - test_message_header_size,
+
+    libspdm_copy_mem(scratch_buffer + test_message_header_size,
+                     scratch_buffer_size - test_message_header_size,
                      (uint8_t *)spdm_test_context->test_buffer,
                      spdm_response_size);
 
@@ -110,7 +128,6 @@ void libspdm_test_requester_psk_finish_case1(void **State)
     size_t hash_size;
     libspdm_session_info_t *session_info;
 
-    m_libspdm_test_message_header = LIBSPDM_TEST_MESSAGE_TYPE_SECURED_TEST;
     spdm_test_context = *State;
     spdm_context = spdm_test_context->spdm_context;
     spdm_context->connection_info.version = SPDM_MESSAGE_VERSION_11
