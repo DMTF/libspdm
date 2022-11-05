e284b160b9c089ad80e37e29fd5817612ce5379f
Steven Bellock <sbellock@nvidia.com>
2022-11-05 13:08:35 -0700

    Fix build failure in get_measurements when debug is disabled
    Signed-off-by: Steven Bellock <sbellock@nvidia.com>

--- library/spdm_requester_lib/libspdm_req_get_measurements.c
+++ library/spdm_requester_lib/libspdm_req_get_measurements.c
@@ -202,7 +202,6 @@ static libspdm_return_t libspdm_try_get_measurement(void *context, const uint32_
     uint8_t *ptr;
     void *nonce;
     uint16_t opaque_length;
-    void *opaque;
     void *signature;
     size_t signature_size;
     libspdm_context_t *spdm_context;
@@ -468,10 +467,13 @@ static libspdm_return_t libspdm_try_get_measurement(void *context, const uint32_
             goto receive_done;
         }
 
-        opaque = ptr;
+        LIBSPDM_DEBUG_CODE(
+            void *opaque;
+            opaque = ptr;
+            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO, "opaque (0x%x):\n", opaque_length));
+            LIBSPDM_INTERNAL_DUMP_HEX(opaque, opaque_length);
+        );
         ptr += opaque_length;
-        LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO, "opaque (0x%x):\n", opaque_length));
-        LIBSPDM_INTERNAL_DUMP_HEX(opaque, opaque_length);
 
         signature = ptr;
         LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO, "signature (0x%x):\n", signature_size));
