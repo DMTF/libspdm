cfefe0594b189136acc2000d1de2ee55f00caa64
Steven Bellock <steven.bellock@gmail.com>
2022-12-01 08:00:16 -0800

    Fix grammar in header file
    Signed-off-by: Steven Bellock <sbellock@nvidia.com>

--- include/internal/libspdm_common_lib.h
+++ include/internal/libspdm_common_lib.h
@@ -1030,12 +1030,12 @@ libspdm_return_t libspdm_append_message_f(void *context, void *spdm_session_info
                                           size_t message_size);
 
 /**
- * This function generate a new session ID by concatnating req_session_id and rsp_session_id.
+ * This function generates a session ID by concatenating req_session_id and rsp_session_id.
  *
  * @param[in]  req_session_id
  * @param[in]  rsp_session_id
  *
- * @return this new session ID.
+ * @return Session ID.
  **/
 uint32_t libspdm_generate_session_id(uint16_t req_session_id, uint16_t rsp_session_id);
 
