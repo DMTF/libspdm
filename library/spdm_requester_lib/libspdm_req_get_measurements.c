bed323f81d72bae92e34121d76d5b3685a3c8d89
Xiangfei Ma <xiangfei.ma@samsung.com>
2023-01-12 10:46:57 +0900

    Correct slot_id check
      Fix: #1621
      Correct slot_id check in libspdm_verify_measurement_signature()
      to prevent asym context memory leaking.
    
    Signed-off-by: Xiangfei Ma <xiangfei.ma@samsung.com>

--- library/spdm_requester_lib/libspdm_req_get_measurements.c
+++ library/spdm_requester_lib/libspdm_req_get_measurements.c
@@ -95,7 +95,7 @@ bool libspdm_verify_measurement_signature(libspdm_context_t *spdm_context,
         spdm_context->connection_info.algorithm.base_asym_algo,
         spdm_context->connection_info.algorithm.base_hash_algo,
         context, l1l2_hash, l1l2_hash_size, sign_data, sign_data_size);
-    if (slot_id == 0xFF) {
+    if (slot_id == 0xF) {
         libspdm_asym_free(spdm_context->connection_info.algorithm.base_asym_algo, context);
     }
 #endif
