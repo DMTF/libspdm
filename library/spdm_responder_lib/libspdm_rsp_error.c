c1928f6d904b287da7b114cbc729c4152ecea008
Qi Zhang <qi1.zhang@intel.com>
2023-01-18 08:22:06 +0800

    spdm_responder_lib: Context Parameter Consistency
    Signed-off-by: Qi Zhang <qi1.zhang@intel.com>

--- library/spdm_responder_lib/libspdm_rsp_error.c
+++ library/spdm_responder_lib/libspdm_rsp_error.c
@@ -6,7 +6,7 @@
 
 #include "internal/libspdm_responder_lib.h"
 
-libspdm_return_t libspdm_generate_error_response(const void *context,
+libspdm_return_t libspdm_generate_error_response(const void *spdm_context,
                                                  uint8_t error_code,
                                                  uint8_t error_data,
                                                  size_t *response_size,
@@ -18,7 +18,7 @@ libspdm_return_t libspdm_generate_error_response(const void *context,
     *response_size = sizeof(spdm_error_response_t);
     spdm_response = response;
 
-    spdm_response->header.spdm_version = libspdm_get_connection_version (context);
+    spdm_response->header.spdm_version = libspdm_get_connection_version (spdm_context);
     if (spdm_response->header.spdm_version == 0) {
         /* if version is not negotiated, then use default version 1.0 */
         spdm_response->header.spdm_version = SPDM_MESSAGE_VERSION_10;
@@ -31,7 +31,7 @@ libspdm_return_t libspdm_generate_error_response(const void *context,
 }
 
 libspdm_return_t libspdm_generate_extended_error_response(
-    const void *context, uint8_t error_code, uint8_t error_data,
+    const void *spdm_context, uint8_t error_code, uint8_t error_data,
     size_t extended_error_data_size, const uint8_t *extended_error_data,
     size_t *response_size, void *response)
 {
@@ -44,7 +44,7 @@ libspdm_return_t libspdm_generate_extended_error_response(
     *response_size = sizeof(spdm_error_response_t) + extended_error_data_size;
     spdm_response = response;
 
-    spdm_response->header.spdm_version = libspdm_get_connection_version (context);
+    spdm_response->header.spdm_version = libspdm_get_connection_version (spdm_context);
     spdm_response->header.request_response_code = SPDM_ERROR;
     spdm_response->header.param1 = error_code;
     spdm_response->header.param2 = error_data;
