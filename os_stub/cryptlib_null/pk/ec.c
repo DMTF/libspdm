e963eafbeaf926405242176a373c4e827e4e8fb3
Wenxing Hou <wenxing.hou@intel.com>
2023-03-28 11:24:17 +0800

    Add ec function in cryptlib_null to pass build
    Signed-off-by: Wenxing Hou <wenxing.hou@intel.com>

--- os_stub/cryptlib_null/pk/ec.c
+++ os_stub/cryptlib_null/pk/ec.c
@@ -41,6 +41,77 @@ void libspdm_ec_free(void *ec_context)
 }
 
 /**
+ * Sets the public key component into the established EC context.
+ *
+ * For P-256, the public_size is 64. first 32-byte is X, second 32-byte is Y.
+ * For P-384, the public_size is 96. first 48-byte is X, second 48-byte is Y.
+ * For P-521, the public_size is 132. first 66-byte is X, second 66-byte is Y.
+ *
+ * @param[in, out]  ec_context      Pointer to EC context being set.
+ * @param[in]       public         Pointer to the buffer to receive generated public X,Y.
+ * @param[in]       public_size     The size of public buffer in bytes.
+ *
+ * @retval  true   EC public key component was set successfully.
+ * @retval  false  Invalid EC public key component.
+ *
+ **/
+bool libspdm_ec_set_pub_key(void *ec_context, const uint8_t *public_key,
+                            size_t public_key_size)
+{
+    LIBSPDM_ASSERT(false);
+    return false;
+}
+
+/**
+ * Sets the private key component into the established EC context.
+ *
+ * For P-256, the private_key_size is 32 byte.
+ * For P-384, the private_key_size is 48 byte.
+ * For P-521, the private_key_size is 66 byte.
+ *
+ * @param[in, out]  ec_context       Pointer to EC context being set.
+ * @param[in]       private_key      Pointer to the private key buffer.
+ * @param[in]       private_key_size The size of private key buffer in bytes.
+ *
+ * @retval  true   EC private key component was set successfully.
+ * @retval  false  Invalid EC private key component.
+ *
+ **/
+bool libspdm_ec_set_priv_key(void *ec_context, const uint8_t *private_key,
+                             size_t private_key_size)
+{
+    LIBSPDM_ASSERT(false);
+    return false;
+}
+
+#if LIBSPDM_FIPS_MODE
+/**
+ * Carries out the EC-DSA signature with caller input random function. This API can be used for FIPS test.
+ *
+ * @param[in]       ec_context    Pointer to EC context for signature generation.
+ * @param[in]       hash_nid      hash NID
+ * @param[in]       message_hash  Pointer to octet message hash to be signed.
+ * @param[in]       hash_size     Size of the message hash in bytes.
+ * @param[out]      signature     Pointer to buffer to receive EC-DSA signature.
+ * @param[in, out]  sig_size      On input, the size of signature buffer in bytes.
+ *                                On output, the size of data returned in signature buffer in bytes.
+ * @param[in]       random_func   random number function
+ *
+ * @retval  true   signature successfully generated in EC-DSA.
+ * @retval  false  signature generation failed.
+ * @retval  false  sig_size is too small.
+ **/
+bool libspdm_ecdsa_sign_ex(void *ec_context, size_t hash_nid,
+                           const uint8_t *message_hash, size_t hash_size,
+                           uint8_t *signature, size_t *sig_size,
+                           int (*random_func)(void *, unsigned char *, size_t))
+{
+    LIBSPDM_ASSERT(false);
+    return false;
+}
+#endif/*LIBSPDM_FIPS_MODE*/
+
+/**
  * Generates EC key and returns EC public key (X, Y).
  *
  * This function generates random secret, and computes the public key (X, Y), which is
