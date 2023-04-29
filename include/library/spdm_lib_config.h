043f81877e435c7a84d3134128f566edfdddefbb
Jiewen Yao <jiewen.yao@intel.com>
2023-04-29 23:15:42 +0800

    move LIBSPDM_MAX_MESSAGE_x_BUFFER_SIZE out of config.h
    That is because it is not configurable.
    
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- include/library/spdm_lib_config.h
+++ include/library/spdm_lib_config.h
@@ -90,20 +90,6 @@
 #define LIBSPDM_RECORD_TRANSCRIPT_DATA_SUPPORT 0
 #endif
 
-/*
- * +--------------------------+------------------------------------------+---------+
- * | GET_VERSION              | 4                                        | 1       |
- * | VERSION {1.0, 1.1, 1.2}  | 6 + 2 * 3 = 12                           | 1       |
- * +--------------------------+------------------------------------------+---------+
- * | GET_CAPABILITIES 1.2     | 20                                       | 1       |
- * | CAPABILITIES 1.2         | 20                                       | 1       |
- * +--------------------------+------------------------------------------+---------+
- * | NEGOTIATE_ALGORITHMS 1.2 | 32 + 4 * 4 = 48                          | 2       |
- * | ALGORITHMS 1.2           | 36 + 4 * 4 = 52                          | 2       |
- * +--------------------------+------------------------------------------+---------+
- */
-#define LIBSPDM_MAX_MESSAGE_VCA_BUFFER_SIZE (150 + 2 * LIBSPDM_MAX_VERSION_COUNT)
-
 /* Cryptography Configuration
  * In each category, at least one should be selected.
  * NOTE: Not all combination can be supported. E.g. Don't mix NIST algo with SMx.*/
