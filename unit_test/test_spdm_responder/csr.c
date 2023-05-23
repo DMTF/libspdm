79bd5fea2411d9634ceab7bf26ce3ae42bb10860
Xiaohanjlll <hanx.xiao@intel.com>
2023-05-23 11:47:56 +0800

    add unit test
    Signed-off-by: Xiaohanjlll <hanx.xiao@intel.com>

--- unit_test/test_spdm_responder/csr.c
+++ unit_test/test_spdm_responder/csr.c
@@ -585,6 +585,150 @@ void libspdm_test_responder_csr_case6(void **state)
     free(m_libspdm_get_csr_request);
 }
 
+/**
+ * Test 7: receives a valid GET_CSR request message from Requester with non-null right req_info and opaque_data
+ * Expected Behavior: produces a valid CSR response message
+ **/
+void libspdm_test_responder_csr_case7(void **state)
+{
+    libspdm_return_t status;
+    libspdm_test_context_t *spdm_test_context;
+    libspdm_context_t *spdm_context;
+    size_t response_size;
+    uint8_t response[LIBSPDM_MAX_SPDM_MSG_SIZE];
+    spdm_csr_response_t *spdm_response;
+    spdm_get_csr_request_t *libspdm_get_csr_request;
+    uint8_t wrong_csr[LIBSPDM_MAX_CSR_SIZE];
+    libspdm_zero_mem(wrong_csr, LIBSPDM_MAX_CSR_SIZE);
+    char *csr;
+
+    spdm_test_context = *state;
+    spdm_context = spdm_test_context->spdm_context;
+    spdm_test_context->case_id = 0x7;
+    spdm_context->connection_info.version = SPDM_MESSAGE_VERSION_12 <<
+                                            SPDM_VERSION_NUMBER_SHIFT_BIT;
+
+    spdm_context->connection_info.connection_state =
+        LIBSPDM_CONNECTION_STATE_NEGOTIATED;
+    spdm_context->local_context.capability.flags = 0;
+    spdm_context->local_context.capability.flags |=
+        SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CSR_CAP;
+    spdm_context->connection_info.algorithm.base_hash_algo =
+        m_libspdm_use_hash_algo;
+    spdm_context->connection_info.algorithm.base_asym_algo =
+        m_libspdm_use_asym_algo;
+
+    libspdm_get_csr_request = malloc(sizeof(spdm_get_csr_request_t) +
+                                     sizeof(m_csr_opaque_data) +
+                                     sizeof(right_req_info));
+
+    libspdm_get_csr_request->header.spdm_version = SPDM_MESSAGE_VERSION_12;
+    libspdm_get_csr_request->header.request_response_code = SPDM_GET_CSR;
+    libspdm_get_csr_request->header.param1 = 0;
+    libspdm_get_csr_request->header.param2 = 0;
+    libspdm_get_csr_request->opaque_data_length = sizeof(m_csr_opaque_data);
+    libspdm_get_csr_request->requester_info_length = sizeof(right_req_info);
+
+    libspdm_copy_mem(libspdm_get_csr_request + 1, sizeof(right_req_info),
+                     right_req_info, sizeof(right_req_info));
+
+    libspdm_copy_mem((uint8_t *)(libspdm_get_csr_request + 1) + sizeof(m_csr_opaque_data),
+                     sizeof(m_csr_opaque_data),
+                     m_csr_opaque_data, sizeof(m_csr_opaque_data));
+
+    size_t libspdm_get_csr_request_size = sizeof(spdm_get_csr_request_t) +
+                                          sizeof(m_csr_opaque_data) +
+                                          sizeof(right_req_info);
+
+    response_size = sizeof(response);
+    status = libspdm_get_response_csr(spdm_context,
+                                      libspdm_get_csr_request_size,
+                                      libspdm_get_csr_request,
+                                      &response_size, response);
+    assert_int_equal(status, LIBSPDM_STATUS_SUCCESS);
+
+    spdm_response = (void *)response;
+    assert_int_equal(response_size, sizeof(spdm_csr_response_t) + spdm_response->csr_length);
+    assert_int_equal(spdm_response->header.request_response_code,
+                     SPDM_CSR);
+
+    /*check returned CSR not zero */
+    assert_memory_not_equal(spdm_response + 1, wrong_csr, spdm_response->csr_length);
+
+    csr = (char *)(spdm_response + 1);
+    assert_true(libspdm_find_buffer(csr, spdm_response->csr_length,
+                                    right_req_info_string, sizeof(right_req_info_string)));
+
+    free(libspdm_get_csr_request);
+}
+
+/**
+ * Test 8: receives a invalid GET_CSR request message from Requester With chaotic req_info and opaque_data
+ * Expected Behavior: generate an ERROR_RESPONSE with code SPDM_ERROR_CODE_INVALID_REQUEST
+ **/
+void libspdm_test_responder_csr_case8(void **state)
+{
+    libspdm_return_t status;
+    libspdm_test_context_t *spdm_test_context;
+    libspdm_context_t *spdm_context;
+    size_t response_size;
+    uint8_t response[LIBSPDM_MAX_SPDM_MSG_SIZE];
+    spdm_csr_response_t *spdm_response;
+    spdm_get_csr_request_t *libspdm_get_csr_request;
+    uint8_t wrong_csr[LIBSPDM_MAX_CSR_SIZE];
+    libspdm_zero_mem(wrong_csr, LIBSPDM_MAX_CSR_SIZE);
+
+    spdm_test_context = *state;
+    spdm_context = spdm_test_context->spdm_context;
+    spdm_test_context->case_id = 0x8;
+    spdm_context->connection_info.version = SPDM_MESSAGE_VERSION_12 <<
+                                            SPDM_VERSION_NUMBER_SHIFT_BIT;
+
+    spdm_context->connection_info.connection_state =
+        LIBSPDM_CONNECTION_STATE_NEGOTIATED;
+    spdm_context->local_context.capability.flags = 0;
+    spdm_context->local_context.capability.flags |=
+        SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CSR_CAP;
+    spdm_context->connection_info.algorithm.base_hash_algo =
+        m_libspdm_use_hash_algo;
+    spdm_context->connection_info.algorithm.base_asym_algo =
+        m_libspdm_use_asym_algo;
+
+    libspdm_get_csr_request = malloc(LIBSPDM_RECEIVER_BUFFER_SIZE);
+    libspdm_get_csr_request->header.spdm_version = SPDM_MESSAGE_VERSION_12;
+    libspdm_get_csr_request->header.request_response_code = SPDM_GET_CSR;
+    libspdm_get_csr_request->header.param1 = 0;
+    libspdm_get_csr_request->header.param2 = 0;
+
+    /* Swap right_req_info and m_csr_opaque_data */
+    libspdm_get_csr_request->opaque_data_length = sizeof(right_req_info);
+    libspdm_get_csr_request->requester_info_length = sizeof(m_csr_opaque_data);
+    libspdm_copy_mem(libspdm_get_csr_request + 1, sizeof(m_csr_opaque_data),
+                     m_csr_opaque_data, sizeof(m_csr_opaque_data));
+    libspdm_copy_mem((uint8_t *)(libspdm_get_csr_request + 1) + sizeof(right_req_info),
+                     sizeof(right_req_info),
+                     right_req_info, sizeof(right_req_info));
+
+    size_t libspdm_get_csr_request_size = sizeof(spdm_get_csr_request_t) +
+                                          sizeof(m_csr_opaque_data) +
+                                          sizeof(right_req_info);
+
+    response_size = sizeof(response);
+    status = libspdm_get_response_csr(spdm_context,
+                                      libspdm_get_csr_request_size,
+                                      libspdm_get_csr_request,
+                                      &response_size, response);
+    assert_int_equal(status, LIBSPDM_STATUS_SUCCESS);
+    assert_int_equal(response_size, sizeof(spdm_error_response_t));
+    spdm_response = (void *)response;
+    assert_int_equal(spdm_response->header.request_response_code,
+                     SPDM_ERROR);
+    assert_int_equal(spdm_response->header.param1,
+                     SPDM_ERROR_CODE_INVALID_REQUEST);
+    assert_int_equal(spdm_response->header.param2, 0);
+    free(libspdm_get_csr_request);
+}
+
 libspdm_test_context_t m_libspdm_responder_csr_test_context = {
     LIBSPDM_TEST_CONTEXT_VERSION,
     false,
@@ -605,6 +749,10 @@ int libspdm_responder_csr_test_main(void)
         cmocka_unit_test(libspdm_test_responder_csr_case5),
         /* Responder need reset to gen csr*/
         cmocka_unit_test(libspdm_test_responder_csr_case6),
+        /* Success Case for csr response with non-null right req_info and opaque_data */
+        cmocka_unit_test(libspdm_test_responder_csr_case7),
+        /* Failed Case for csr response  With chaotic req_info and opaque_data */
+        cmocka_unit_test(libspdm_test_responder_csr_case8),
     };
 
     libspdm_setup_test_context(&m_libspdm_responder_csr_test_context);
