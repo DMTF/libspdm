2332bc15dacb425d90940267e7df7387c5402f88
Steven Bellock <sbellock@nvidia.com>
2023-09-14 13:46:44 -0700

    Add little-endian to big-endian function
    Signed-off-by: Steven Bellock <sbellock@nvidia.com>

--- include/internal/libspdm_common_lib.h
+++ include/internal/libspdm_common_lib.h
@@ -1585,4 +1585,16 @@ static inline bool libspdm_onehot0(uint32_t mask)
     return !mask || !(mask & (mask - 1));
 }
 
+static inline uint64_t libspdm_le_to_be_64(uint64_t value)
+{
+    return (((value & 0x00000000000000ff) << 56) |
+            ((value & 0x000000000000ff00) << 40) |
+            ((value & 0x0000000000ff0000) << 24) |
+            ((value & 0x00000000ff000000) << 8) |
+            ((value & 0x000000ff00000000) >> 8) |
+            ((value & 0x0000ff0000000000) >> 24) |
+            ((value & 0x00ff000000000000) >> 40) |
+            ((value & 0xff00000000000000) >> 56));
+}
+
 #endif /* SPDM_COMMON_LIB_INTERNAL_H */
