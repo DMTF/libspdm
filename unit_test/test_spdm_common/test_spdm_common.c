e601d71f92d0d8f9dc7356387df5e03a74d9e139
Steven Bellock <sbellock@nvidia.com>
2023-09-14 13:46:44 -0700

    Add little-endian to big-endian function
    Signed-off-by: Steven Bellock <sbellock@nvidia.com>

--- unit_test/test_spdm_common/test_spdm_common.c
+++ unit_test/test_spdm_common/test_spdm_common.c
@@ -6,12 +6,17 @@
 
 
 extern int libspdm_common_context_data_test_main(void);
+extern int libspdm_common_support_test_main(void);
 
 int main(void)
 {
     int return_value = 0;
 
     if (libspdm_common_context_data_test_main() != 0) {
+        return_value = 1;
+    }
+
+    if (libspdm_common_support_test_main() != 0) {
         return_value = 1;
     }
 
