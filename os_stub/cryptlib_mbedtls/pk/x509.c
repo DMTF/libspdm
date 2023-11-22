DMTF/libspdm

commit 6f798df235c7e8fe6cc5eaa40bb5dc509ca7de51

os_stub: cryptlib: CSR: Allow copying attributes from an existing cert
Section 754 of SPDM 1.3 states that:

    The CSR shall be populated with a combination of attributes
    provided by the Requester via the RequesterInfo field and other
    attributes contributed by the Responder itself.

libspdm currently supports populating the CSR based on the
RequesterInfo, but we don't allow the Responder to add custom
attributes.

This patch updated libspdm_gen_x509_csr() to pass in a base certificate
that is used to provide CSR attributes. libspdm_gen_x509_csr() will copy
the extensions from base_cert to the CSR.

This way devices can choose to apply the same extensions from a leaf
certificate (or other certificate already on the device) or create a new
cert just to pass the extensions in.

By copying the extensions from an existing cert the API should be
generic between OpenSSL and MBedTLS implementations.

Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

os_stub/cryptlib_mbedtls/pk/x509.c
@@ -1931,6 +1931,8 @@ bool libspdm_set_attribute_for_req(mbedtls_x509write_csr *req,
  * @param[in, out]      csr_pointer           For input, csr_pointer is buffer address to store CSR.
  *                                            For output, csr_pointer is address for stored CSR.
  *                                            The csr_pointer address will be changed.
+ * @param[in]           base_cert             An optional leaf certificate whose
+ *                                            extensions should be copied to the CSR
  *
  * @retval  true   Success.
  * @retval  false  Failed to gen CSR.
@@ -1939,7 +1941,8 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
                           uint8_t *requester_info, size_t requester_info_length,
                           bool is_ca,
                           void *context, char *subject_name,
-                          size_t *csr_len, uint8_t *csr_pointer)
+                          size_t *csr_len, uint8_t *csr_pointer,
+                          void *base_cert)
 {
     int ret;
     bool result;
@@ -1947,11 +1950,16 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
 
     mbedtls_x509write_csr req;
     mbedtls_md_type_t md_alg;
+    mbedtls_asn1_sequence extns;
+    mbedtls_asn1_sequence *next;
+    mbedtls_x509_buf buf;
+    mbedtls_x509_crt *cert;
     mbedtls_pk_context key;
 
     uint8_t pubkey_buffer[LIBSPDM_MAX_PUBKEY_DER_BUFFER_SIZE];
     uint8_t *pubkey_der_data;
     size_t pubkey_der_len;
+    size_t tag_len;
 
     /*basic_constraints: CA: false */
     #define BASIC_CONSTRAINTS_STRING_FALSE {0x30, 0x00}
@@ -1965,6 +1973,7 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
     mbedtls_x509write_csr_init(&req);
     mbedtls_pk_init(&key);
     csr_buffer_size = *csr_len;
+    next = NULL;
 
     ret = 1;
     switch (asym_nid)
@@ -2063,6 +2072,43 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
 
     /* Set key */
     mbedtls_x509write_csr_set_key(&req, &key);
+
+    if (base_cert != NULL) {
+        cert = base_cert;
+        buf = cert->v3_ext;
+        if (mbedtls_asn1_get_sequence_of(&buf.p, buf.p + buf.len, &extns,
+                                         MBEDTLS_ASN1_CONSTRUCTED | MBEDTLS_ASN1_SEQUENCE)) {
+            ret = 1;
+            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
+                           "mbedtls_x509write_csr_set_extension unable to get tag\n"));
+            goto free_all;
+        }
+
+        next = &extns;
+    }
+
+    while (next) {
+        if (mbedtls_asn1_get_tag(&(next->buf.p), next->buf.p + next->buf.len, &tag_len,
+                                 MBEDTLS_ASN1_OID)) {
+            ret = 1;
+            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
+                           "mbedtls_x509write_csr_set_extension unable to get tag\n"));
+            goto free_all;
+        }
+
+        if (mbedtls_x509write_csr_set_extension(&req, MBEDTLS_OID_BASIC_CONSTRAINTS,
+                                                MBEDTLS_OID_SIZE(MBEDTLS_OID_BASIC_CONSTRAINTS),
+                                                next->buf.p,
+                                                tag_len
+                                                ) != 0) {
+            ret = 1;
+            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
+                           "mbedtls_x509write_csr_set_extension set custom OID failed \n"));
+            goto free_all;
+        }
+
+        next = next->next;
+    }
 
     /*set basicConstraints*/
     if (mbedtls_x509write_csr_set_extension(&req, MBEDTLS_OID_BASIC_CONSTRAINTS,

os_stub/cryptlib_null/pk/x509.c
@@ -766,6 +766,8 @@ int32_t libspdm_x509_compare_date_time(const void *date_time1, const void *date_
  * @param[in, out]      csr_pointer           For input, csr_pointer is buffer address to store CSR.
  *                                            For output, csr_pointer is address for stored CSR.
  *                                            The csr_pointer address will be changed.
+ * @param[in]           base_cert             An optional leaf certificate whose
+ *                                            extensions should be copied to the CSR
  *
  * @retval  true   Success.
  * @retval  false  Failed to gen CSR.
@@ -774,7 +776,8 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
                           uint8_t *requester_info, size_t requester_info_length,
                           bool is_ca,
                           void *context, char *subject_name,
-                          size_t *csr_len, uint8_t *csr_pointer)
+                          size_t *csr_len, uint8_t *csr_pointer,
+                          void *base_cert)
 {
     LIBSPDM_ASSERT(false);
     return false;

os_stub/cryptlib_openssl/pk/x509.c
@@ -2460,6 +2460,8 @@ bool libspdm_set_attribute_for_req(X509_REQ *req, uint8_t *req_info, size_t req_
  * @param[in, out]      csr_pointer           For input, csr_pointer is buffer address to store CSR.
  *                                            For output, csr_pointer is address for stored CSR.
  *                                            The csr_pointer address will be changed.
+ * @param[in]           base_cert             An optional leaf certificate whose
+ *                                            extensions should be copied to the CSR
  *
  * @retval  true   Success.
  * @retval  false  Failed to gen CSR.
@@ -2468,7 +2470,8 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
                           uint8_t *requester_info, size_t requester_info_length,
                           bool is_ca,
                           void *context, char *subject_name,
-                          size_t *csr_len, uint8_t *csr_pointer)
+                          size_t *csr_len, uint8_t *csr_pointer,
+                          void *base_cert)
 {
     int ret;
     int version;
@@ -2483,6 +2486,7 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
     uint8_t *csr_p;
     STACK_OF(X509_EXTENSION) *exts;
     X509_EXTENSION *basic_constraints_ext;
+    int num_exts;
 
     exts = NULL;
     basic_constraints_ext = NULL;
@@ -2497,6 +2501,7 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
     ec_public_key = NULL;
     md = NULL;
     csr_p = csr_pointer;
+    num_exts = 0;
 
     x509_req = X509_REQ_new();
     if (x509_req == NULL) {
@@ -2618,6 +2623,7 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
         goto free_all;
     }
 
+    /*set basicConstraints*/
     basic_constraints_ext = X509V3_EXT_conf_nid(
         NULL, NULL, NID_basic_constraints,
         is_ca ? "CA:TRUE" : "CA:FALSE");
@@ -2626,9 +2632,16 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
         ret = 0;
         goto free_all;
     }
-
-    /*set basicConstraints*/
     sk_X509_EXTENSION_push(exts, basic_constraints_ext);
+
+    if (base_cert != NULL) {
+        num_exts = X509_get_ext_count(base_cert);
+
+        for (int i = 0; i < num_exts; i++) {
+            sk_X509_EXTENSION_push(exts, X509_get_ext(base_cert, i));
+        }
+    }
+
     X509_REQ_add_extensions(x509_req, exts);
     sk_X509_EXTENSION_free(exts);
 

os_stub/spdm_crypt_ext_lib/cryptlib_ext.h
@@ -739,6 +739,8 @@ extern bool libspdm_sm2_dsa_generate_key(void *sm2_context, uint8_t *public_data
  * @param[in, out]      csr_pointer           For input, csr_pointer is buffer address to store CSR.
  *                                            For output, csr_pointer is address for stored CSR.
  *                                            The csr_pointer address will be changed.
+ * @param[in]           base_cert             An optional leaf certificate whose
+ *                                            extensions should be copied to the CSR
  *
  * @retval  true   Success.
  * @retval  false  Failed to gen CSR.
@@ -747,7 +749,8 @@ extern bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
                                  uint8_t *requester_info, size_t requester_info_length,
                                  bool is_ca,
                                  void *context, char *subject_name,
-                                 size_t *csr_len, uint8_t *csr_pointer);
+                                 size_t *csr_len, uint8_t *csr_pointer,
+                                 void *base_cert);
 #endif /* LIBSPDM_ENABLE_CAPABILITY_CSR_CAP */
 
 #endif /* CRYPTLIB_EXT_H */

os_stub/spdm_device_secret_lib_sample/lib.c
@@ -688,7 +688,8 @@ bool libspdm_gen_csr(uint32_t base_hash_algo, uint32_t base_asym_algo, bool *nee
                                       requester_info, requester_info_length,
                                       !is_device_cert_model,
                                       context, subject_name,
-                                      csr_len, csr_pointer);
+                                      csr_len, csr_pointer,
+                                      NULL);
         libspdm_asym_free(base_asym_algo, context);
         libspdm_zero_mem(prikey, prikey_size);
         free(prikey);
@@ -707,7 +708,8 @@ bool libspdm_gen_csr(uint32_t base_hash_algo, uint32_t base_asym_algo, bool *nee
                                   requester_info, requester_info_length,
                                   !is_device_cert_model,
                                   context, subject_name,
-                                  csr_len, csr_pointer);
+                                  csr_len, csr_pointer,
+                                  NULL);
     libspdm_asym_free(base_asym_algo, context);
 #if !LIBSPDM_PRIVATE_KEY_MODE_RAW_KEY_ONLY
 }

commit fa9cffa5f3758246be6c697d780c8b028aa471b8

os_stub/cryptlib_mbedtls: x509: Fixup setting the CSR constraints
We were incorrectly not actually copying the OIDs from the existing
cert. This ensures that we do apply the OIDs

Fixes: 6f798df235 "os_stub: cryptlib: CSR: Allow copying attributes from an existing cert"
Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

os_stub/cryptlib_mbedtls/pk/x509.c
@@ -1951,7 +1951,7 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
     mbedtls_x509write_csr req;
     mbedtls_md_type_t md_alg;
     mbedtls_asn1_sequence extns;
-    mbedtls_asn1_sequence *next;
+    mbedtls_asn1_sequence *next_oid;
     mbedtls_x509_buf buf;
     mbedtls_x509_crt *cert;
     mbedtls_pk_context key;
@@ -1959,7 +1959,7 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
     uint8_t pubkey_buffer[LIBSPDM_MAX_PUBKEY_DER_BUFFER_SIZE];
     uint8_t *pubkey_der_data;
     size_t pubkey_der_len;
-    size_t tag_len;
+    size_t oid_tag_len;
 
     /*basic_constraints: CA: false */
     #define BASIC_CONSTRAINTS_STRING_FALSE {0x30, 0x00}
@@ -1973,7 +1973,7 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
     mbedtls_x509write_csr_init(&req);
     mbedtls_pk_init(&key);
     csr_buffer_size = *csr_len;
-    next = NULL;
+    next_oid = NULL;
 
     ret = 1;
     switch (asym_nid)
@@ -2073,43 +2073,6 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
     /* Set key */
     mbedtls_x509write_csr_set_key(&req, &key);
 
-    if (base_cert != NULL) {
-        cert = base_cert;
-        buf = cert->v3_ext;
-        if (mbedtls_asn1_get_sequence_of(&buf.p, buf.p + buf.len, &extns,
-                                         MBEDTLS_ASN1_CONSTRUCTED | MBEDTLS_ASN1_SEQUENCE)) {
-            ret = 1;
-            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
-                           "mbedtls_x509write_csr_set_extension unable to get tag\n"));
-            goto free_all;
-        }
-
-        next = &extns;
-    }
-
-    while (next) {
-        if (mbedtls_asn1_get_tag(&(next->buf.p), next->buf.p + next->buf.len, &tag_len,
-                                 MBEDTLS_ASN1_OID)) {
-            ret = 1;
-            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
-                           "mbedtls_x509write_csr_set_extension unable to get tag\n"));
-            goto free_all;
-        }
-
-        if (mbedtls_x509write_csr_set_extension(&req, MBEDTLS_OID_BASIC_CONSTRAINTS,
-                                                MBEDTLS_OID_SIZE(MBEDTLS_OID_BASIC_CONSTRAINTS),
-                                                next->buf.p,
-                                                tag_len
-                                                ) != 0) {
-            ret = 1;
-            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
-                           "mbedtls_x509write_csr_set_extension set custom OID failed \n"));
-            goto free_all;
-        }
-
-        next = next->next;
-    }
-
     /*set basicConstraints*/
     if (mbedtls_x509write_csr_set_extension(&req, MBEDTLS_OID_BASIC_CONSTRAINTS,
                                             MBEDTLS_OID_SIZE(MBEDTLS_OID_BASIC_CONSTRAINTS),
@@ -2122,6 +2085,43 @@ bool libspdm_gen_x509_csr(size_t hash_nid, size_t asym_nid,
         LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
                        "mbedtls_x509write_csr_set_extension set basicConstraints failed \n"));
         goto free_all;
+    }
+
+    if (base_cert != NULL) {
+        cert = base_cert;
+        buf = cert->v3_ext;
+        if (mbedtls_asn1_get_sequence_of(&buf.p, buf.p + buf.len, &extns,
+                                         MBEDTLS_ASN1_CONSTRUCTED | MBEDTLS_ASN1_SEQUENCE)) {
+            ret = 1;
+            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
+                           "mbedtls_x509write_csr_set_extension unable to get sequence\n"));
+            goto free_all;
+        }
+
+        next_oid = &extns;
+    }
+
+    while (next_oid) {
+        if (mbedtls_asn1_get_tag(&(next_oid->buf.p), next_oid->buf.p + next_oid->buf.len,
+                                 &oid_tag_len, MBEDTLS_ASN1_OID)) {
+            ret = 1;
+            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
+                           "mbedtls_x509write_csr_set_extension unable to get OID tag\n"));
+            goto free_all;
+        }
+
+        if (mbedtls_x509write_csr_set_extension(&req, next_oid->buf.p,
+                                                oid_tag_len,
+                                                next_oid->buf.p + oid_tag_len,
+                                                next_oid->buf.len - oid_tag_len
+                                                ) != 0) {
+            ret = 1;
+            LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
+                           "mbedtls_x509write_csr_set_extension set custom OID failed \n"));
+            goto free_all;
+        }
+
+        next_oid = next_oid->next;
     }
 
     /*csr data is written at the end of the buffer*/