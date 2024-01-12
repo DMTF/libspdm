21f362f68f687a95d08ae5be56f5f9d1ba45a6fb
Jiewen Yao <jiewen.yao@intel.com>
2024-01-12 19:35:20 +0800

    Add check for KeyUsage bit
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- library/spdm_responder_lib/libspdm_rsp_measurements.c
+++ library/spdm_responder_lib/libspdm_rsp_measurements.c
@@ -383,6 +383,17 @@ libspdm_return_t libspdm_get_response_measurements(libspdm_context_t *spdm_conte
                         0, response_size, response);
                 }
             }
+
+            if ((spdm_request->header.spdm_version >= SPDM_MESSAGE_VERSION_13) &&
+                spdm_context->connection_info.multi_key_conn_rsp) {
+                if ((spdm_context->local_context.local_key_usage_bit_mask[slot_id_param] &
+                     SPDM_KEY_USAGE_BIT_MASK_MEASUREMENT_USE) == 0) {
+                    return libspdm_generate_error_response(
+                        spdm_context, SPDM_ERROR_CODE_INVALID_REQUEST,
+                        0, response_size, response);
+                }
+            }
+
             spdm_response->header.param2 = slot_id_param;
             if (spdm_response->header.spdm_version >= SPDM_MESSAGE_VERSION_12) {
                 spdm_response->header.param2 = slot_id_param |
