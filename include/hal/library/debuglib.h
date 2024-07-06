5a65cf02934de8771888cdf29bab55290e6619f3
Steven Bellock <sbellock@nvidia.com>
2024-07-06 15:53:19 -0700

    Remove unused variable
    Fix #2686.
    
    Signed-off-by: Steven Bellock <sbellock@nvidia.com>

--- include/hal/library/debuglib.h
+++ include/hal/library/debuglib.h
@@ -106,10 +106,7 @@ extern void libspdm_debug_assert(const char *file_name, size_t line_number,
 #if LIBSPDM_DEBUG_BLOCK_ENABLE
 #define LIBSPDM_DEBUG_CODE(expression) \
     do { \
-        uint8_t __debug_code_local; \
         expression \
-            __debug_code_local = 0; \
-        __debug_code_local++; \
     } while (false)
 #else
 #define LIBSPDM_DEBUG_CODE(expression)
