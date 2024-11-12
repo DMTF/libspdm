a0888c0817c1657046218fff9d83c52a89747c65
Jiewen Yao <jiewen.yao@intel.com>
2024-11-12 11:27:16 +0800

    1.4 add PQC algorithm in configuration
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- include/internal/libspdm_lib_config.h
+++ include/internal/libspdm_lib_config.h
@@ -1,6 +1,6 @@
 /**
  *  Copyright Notice:
- *  Copyright 2021-2024 DMTF. All rights reserved.
+ *  Copyright 2021-2025 DMTF. All rights reserved.
  *  License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
  **/
 
@@ -83,6 +83,27 @@
                               (LIBSPDM_SHA3_512_SUPPORT))
 
 #define LIBSPDM_SM3_SUPPORT (LIBSPDM_SM3_256_SUPPORT)
+
+#define LIBSPDM_ML_DSA_SUPPORT ((LIBSPDM_ML_DSA_44_SUPPORT) || \
+                                (LIBSPDM_ML_DSA_65_SUPPORT) || \
+                                (LIBSPDM_ML_DSA_87_SUPPORT))
+
+#define LIBSPDM_SLH_DSA_SUPPORT ((LIBSPDM_SLH_DSA_SHA2_128S_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHAKE_128S_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHA2_128F_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHAKE_128F_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHA2_192S_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHAKE_192S_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHA2_192F_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHAKE_192F_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHA2_256S_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHAKE_256S_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHA2_256F_SUPPORT) || \
+                                 (LIBSPDM_SLH_DSA_SHAKE_256F_SUPPORT))
+
+#define LIBSPDM_ML_KEM_SUPPORT ((LIBSPDM_ML_KEM_512_SUPPORT) || \
+                                (LIBSPDM_ML_KEM_768_SUPPORT) || \
+                                (LIBSPDM_ML_KEM_1024_SUPPORT))
 
 #if LIBSPDM_CHECK_MACRO
 #include "internal/libspdm_macro_check.h"
