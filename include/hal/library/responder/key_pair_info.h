69c7dd2d4828ce6bfbdc65ba517786cfb062cd0b
Jiewen Yao <jiewen.yao@intel.com>
2024-11-27 14:39:35 +0800

    1.4 Add PQC to HAL key_pair_info lib
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- include/hal/library/responder/key_pair_info.h
+++ include/hal/library/responder/key_pair_info.h
@@ -14,6 +14,11 @@
 #if LIBSPDM_ENABLE_CAPABILITY_GET_KEY_PAIR_INFO_CAP
 
 /**
+ * read the total key pairs number.
+ **/
+extern uint8_t libspdm_read_total_key_pairs();
+
+/**
  * read the key pair info of the key_pair_id.
  *
  * @param  spdm_context               A pointer to the SPDM context.
@@ -42,6 +47,8 @@ extern bool libspdm_read_key_pair_info(
     uint16_t *current_key_usage,
     uint32_t *asym_algo_capabilities,
     uint32_t *current_asym_algo,
+    uint32_t *pqc_asym_algo_capabilities,
+    uint32_t *current_pqc_asym_algo,
     uint8_t *assoc_cert_slot_mask,
     uint16_t *public_key_info_len,
     uint8_t *public_key_info);
@@ -73,6 +80,7 @@ extern bool libspdm_write_key_pair_info(
     uint8_t operation,
     uint16_t desired_key_usage,
     uint32_t desired_asym_algo,
+    uint32_t desired_pqc_asym_algo,
     uint8_t desired_assoc_cert_slot_mask,
     bool * need_reset);
 #endif /* LIBSPDM_ENABLE_CAPABILITY_SET_KEY_PAIR_INFO_CAP */
