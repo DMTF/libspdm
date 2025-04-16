4d9d4ee0287bb9bca21e1704e6a464602871cd0f
Aaron Li <aaron.li@intel.com>
2025-04-16 10:53:24 +0800

    upgrade to openssl-3.5.1-dev
    Signed-off-by: Aaron Li <aaron.li@intel.com>

--- os_stub/openssllib/include/openssl/configuration.h
+++ os_stub/openssllib/include/openssl/configuration.h
@@ -89,6 +89,9 @@ typedef int64_t INT64;
 # ifndef OPENSSL_NO_APPS
 #  define OPENSSL_NO_APPS
 # endif
+# ifndef OPENSSL_NO_ARGON2
+#  define OPENSSL_NO_ARGON2
+# endif
 # ifndef OPENSSL_NO_ARIA
 #  define OPENSSL_NO_ARIA
 # endif
@@ -112,6 +115,12 @@ typedef int64_t INT64;
 # endif
 # ifndef OPENSSL_NO_BLAKE2
 #  define OPENSSL_NO_BLAKE2
+# endif
+# ifndef OPENSSL_NO_BROTLI
+#  define OPENSSL_NO_BROTLI
+# endif
+# ifndef OPENSSL_NO_BROTLI_DYNAMIC
+#  define OPENSSL_NO_BROTLI_DYNAMIC
 # endif
 # ifndef OPENSSL_NO_CAMELLIA
 #  define OPENSSL_NO_CAMELLIA
@@ -142,6 +151,12 @@ typedef int64_t INT64;
 # endif
 # ifndef OPENSSL_NO_CT
 #  define OPENSSL_NO_CT
+# endif
+# ifndef OPENSSL_NO_DEFAULT_THREAD_POOL
+#  define OPENSSL_NO_DEFAULT_THREAD_POOL
+# endif
+# ifndef OPENSSL_NO_DEMOS
+#  define OPENSSL_NO_DEMOS
 # endif
 # ifndef OPENSSL_NO_DEPRECATED
 #  define OPENSSL_NO_DEPRECATED
@@ -191,6 +206,12 @@ typedef int64_t INT64;
 # ifndef OPENSSL_NO_FILENAMES
 #  define OPENSSL_NO_FILENAMES
 # endif
+# ifndef OPENSSL_NO_FIPS_JITTER
+#  define OPENSSL_NO_FIPS_JITTER
+# endif
+# ifndef OPENSSL_NO_FIPS_POST
+#  define OPENSSL_NO_FIPS_POST
+# endif
 # ifndef OPENSSL_NO_FIPS_SECURITYCHECKS
 #  define OPENSSL_NO_FIPS_SECURITYCHECKS
 # endif
@@ -203,8 +224,17 @@ typedef int64_t INT64;
 # ifndef OPENSSL_NO_GOST
 #  define OPENSSL_NO_GOST
 # endif
+# ifndef OPENSSL_NO_H3DEMO
+#  define OPENSSL_NO_H3DEMO
+# endif
+# ifndef OPENSSL_NO_HQINTEROP
+#  define OPENSSL_NO_HQINTEROP
+# endif
 # ifndef OPENSSL_NO_IDEA
 #  define OPENSSL_NO_IDEA
+# endif
+# ifndef OPENSSL_NO_JITTER
+#  define OPENSSL_NO_JITTER
 # endif
 # ifndef OPENSSL_NO_KTLS
 #  define OPENSSL_NO_KTLS
@@ -221,6 +251,12 @@ typedef int64_t INT64;
 # ifndef OPENSSL_NO_MDC2
 #  define OPENSSL_NO_MDC2
 # endif
+# ifndef OPENSSL_NO_ML_DSA
+#  define OPENSSL_NO_ML_DSA
+# endif
+# ifndef OPENSSL_NO_ML_KEM
+#  define OPENSSL_NO_ML_KEM
+# endif
 # ifndef OPENSSL_NO_MSAN
 #  define OPENSSL_NO_MSAN
 # endif
@@ -233,8 +269,17 @@ typedef int64_t INT64;
 # ifndef OPENSSL_NO_PADLOCKENG
 #  define OPENSSL_NO_PADLOCKENG
 # endif
+# ifndef OPENSSL_NO_PIE
+#  define OPENSSL_NO_PIE
+# endif
 # ifndef OPENSSL_NO_POSIX_IO
 #  define OPENSSL_NO_POSIX_IO
+# endif
+# ifndef OPENSSL_NO_QLOG
+#  define OPENSSL_NO_QLOG
+# endif
+# ifndef OPENSSL_NO_QUIC
+#  define OPENSSL_NO_QUIC
 # endif
 # ifndef OPENSSL_NO_RC2
 #  define OPENSSL_NO_RC2
@@ -266,6 +311,9 @@ typedef int64_t INT64;
 # ifndef OPENSSL_NO_SIV
 #  define OPENSSL_NO_SIV
 # endif
+# ifndef OPENSSL_NO_SLH_DSA
+#  define OPENSSL_NO_SLH_DSA
+# endif
 # ifndef OPENSSL_NO_SOCK
 #  define OPENSSL_NO_SOCK
 # endif
@@ -284,11 +332,23 @@ typedef int64_t INT64;
 # ifndef OPENSSL_NO_SSL3_METHOD
 #  define OPENSSL_NO_SSL3_METHOD
 # endif
+# ifndef OPENSSL_NO_SSLKEYLOG
+#  define OPENSSL_NO_SSLKEYLOG
+# endif
 # ifndef OPENSSL_NO_STDIO
 #  define OPENSSL_NO_STDIO
 # endif
 # ifndef OPENSSL_NO_TESTS
 #  define OPENSSL_NO_TESTS
+# endif
+# ifndef OPENSSL_NO_TFO
+#  define OPENSSL_NO_TFO
+# endif
+# ifndef OPENSSL_NO_THREAD_POOL
+#  define OPENSSL_NO_THREAD_POOL
+# endif
+# ifndef OPENSSL_NO_TLS_DEPRECATED_EC
+#  define OPENSSL_NO_TLS_DEPRECATED_EC
 # endif
 # ifndef OPENSSL_NO_TRACE
 #  define OPENSSL_NO_TRACE
@@ -305,6 +365,9 @@ typedef int64_t INT64;
 # ifndef OPENSSL_NO_UNIT_TEST
 #  define OPENSSL_NO_UNIT_TEST
 # endif
+# ifndef OPENSSL_NO_UNSTABLE_QLOG
+#  define OPENSSL_NO_UNSTABLE_QLOG
+# endif
 # ifndef OPENSSL_NO_UPLINK
 #  define OPENSSL_NO_UPLINK
 # endif
@@ -313,6 +376,21 @@ typedef int64_t INT64;
 # endif
 # ifndef OPENSSL_NO_WHIRLPOOL
 #  define OPENSSL_NO_WHIRLPOOL
+# endif
+# ifndef OPENSSL_NO_WINSTORE
+#  define OPENSSL_NO_WINSTORE
+# endif
+# ifndef OPENSSL_NO_ZLIB
+#  define OPENSSL_NO_ZLIB
+# endif
+# ifndef OPENSSL_NO_ZLIB_DYNAMIC
+#  define OPENSSL_NO_ZLIB_DYNAMIC
+# endif
+# ifndef OPENSSL_NO_ZSTD
+#  define OPENSSL_NO_ZSTD
+# endif
+# ifndef OPENSSL_NO_ZSTD_DYNAMIC
+#  define OPENSSL_NO_ZSTD_DYNAMIC
 # endif
 # ifndef OPENSSL_NO_DYNAMIC_ENGINE
 #  define OPENSSL_NO_DYNAMIC_ENGINE
@@ -337,6 +415,12 @@ typedef int64_t INT64;
 # endif
 
 # define RC4_INT unsigned int
+
+# if defined(OPENSSL_NO_COMP) || (defined(OPENSSL_NO_BROTLI) && defined(OPENSSL_NO_ZSTD) && defined(OPENSSL_NO_ZLIB))
+#  define OPENSSL_NO_COMP_ALG
+# else
+#  undef  OPENSSL_NO_COMP_ALG
+# endif
 
 # ifdef  __cplusplus
 }
