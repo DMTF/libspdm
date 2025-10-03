f4c992ee31ab3fd1319db1e190a7ca5affb9f825
Steven Bellock <sbellock@nvidia.com>
2025-10-03 10:32:53 -0700

    Update copyright year for renamed files
    Signed-off-by: Steven Bellock <sbellock@nvidia.com>

--- unit_test/test_spdm_responder/supported_event_types.c
+++ unit_test/test_spdm_responder/supported_event_types.c
@@ -1,6 +1,6 @@
 /**
  *  Copyright Notice:
- *  Copyright 2024 DMTF. All rights reserved.
+ *  Copyright 2024-2025 DMTF. All rights reserved.
  *  License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
  **/
 
