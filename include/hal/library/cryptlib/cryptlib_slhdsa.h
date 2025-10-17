e06286dadbde34fc30c31721d4c7e5789246c30b
Aaron Li <aaron.li@intel.com>
2025-10-17 15:22:27 +0800

    Add FIPS self-test for SLH-DSA
    Signed-off-by: Aaron Li <aaron.li@intel.com>

--- include/hal/library/cryptlib/cryptlib_slhdsa.h
+++ include/hal/library/cryptlib/cryptlib_slhdsa.h
@@ -105,6 +105,32 @@ extern bool libspdm_slhdsa_sign(void *dsa_context,
                                 const uint8_t *message, size_t message_size,
                                 uint8_t *signature, size_t *sig_size);
 
+#if LIBSPDM_FIPS_MODE
+/**
+ * Carries out the SLHDSA signature generation. This API can be used for FIPS test.
+ *
+ * @param[in]      dsa_context   Pointer to DSA context for signature generation.
+ * @param[in]      context       The SLHDSA signing context.
+ * @param[in]      context_size  Size of SLHDSA signing context.
+ * @param[in]      message       Pointer to octet message to be signed.
+ * @param[in]      message_size  Size of the message in bytes.
+ * @param[out]     signature     Pointer to buffer to receive DSA signature.
+ * @param[in, out] sig_size      On input, the size of signature buffer in bytes.
+ *                               On output, the size of data returned in signature buffer in bytes.
+ * @param[in]      deterministic If true, then generate the signature in deterministic way.
+ *
+ * @retval  true   signature successfully generated.
+ * @retval  false  signature generation failed.
+ * @retval  false  sig_size is too small.
+ * @retval  false  This interface is not supported.
+ **/
+bool libspdm_slhdsa_sign_ex(void *dsa_context,
+                            const uint8_t *context, size_t context_size,
+                            const uint8_t *message, size_t message_size,
+                            uint8_t *signature, size_t *sig_size,
+                            bool deterministic);
+#endif /* LIBSPDM_FIPS_MODE */
+
 /**
  * Verifies the SLHDSA signature.
  *
