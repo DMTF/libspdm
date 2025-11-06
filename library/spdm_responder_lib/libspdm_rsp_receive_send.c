e18e28755c6289d4140698d232d3f026e32f9337
Aaron Li <aaron.li@intel.com>
2025-11-06 09:34:48 +0800

    erase the plain-text VDM message after processing it.
    fix #2426
    
    Signed-off-by: Aaron Li <aaron.li@intel.com>
    (cherry picked from commit 9bb426f1876a0c8979c25a2cb239db21885621d9)

--- library/spdm_responder_lib/libspdm_rsp_receive_send.c
+++ library/spdm_responder_lib/libspdm_rsp_receive_send.c
@@ -294,6 +294,7 @@ libspdm_return_t libspdm_process_request(void *spdm_context, uint32_t **session_
                       libspdm_get_scratch_buffer_last_spdm_request_capacity(context),
                       decoded_message_ptr,
                       decoded_message_size);
+    libspdm_zero_mem (decoded_message_ptr, decoded_message_size);
 
     if (!(*is_app_message)) {
         /* Check for minimal SPDM message size. */
@@ -733,7 +734,8 @@ libspdm_return_t libspdm_build_response(void *spdm_context, const uint32_t *sess
      * return UNSUPPORTED and clear response_size to continue the dispatch without send response.*/
     if((my_response_size == 0) && (status == LIBSPDM_STATUS_UNSUPPORTED_CAP)) {
         *response_size = 0;
-        return LIBSPDM_STATUS_UNSUPPORTED_CAP;
+        status = LIBSPDM_STATUS_UNSUPPORTED_CAP;
+        goto done;
     }
 
     if (LIBSPDM_STATUS_IS_ERROR(status)) {
@@ -741,7 +743,7 @@ libspdm_return_t libspdm_build_response(void *spdm_context, const uint32_t *sess
             context, SPDM_ERROR_CODE_UNSUPPORTED_REQUEST,
             spdm_request->request_response_code, &my_response_size, my_response);
         if (LIBSPDM_STATUS_IS_ERROR(status)) {
-            return status;
+            goto done;
         }
     }
 
@@ -762,7 +764,7 @@ libspdm_return_t libspdm_build_response(void *spdm_context, const uint32_t *sess
             libspdm_free_session_id(context, *session_id);
         }
         LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO, "transport_encode_message : %xu\n", status));
-        return status;
+        goto done;
     }
 
     request_response_code = spdm_response->request_response_code;
@@ -853,7 +855,17 @@ libspdm_return_t libspdm_build_response(void *spdm_context, const uint32_t *sess
         }
     }
 
-    return LIBSPDM_STATUS_SUCCESS;
+    status = LIBSPDM_STATUS_SUCCESS;
+done:
+    if (session_id != NULL) {
+        /* clean plain text in stratch buffer */
+        libspdm_zero_mem (my_response, my_response_size);
+    }
+    libspdm_zero_mem (context->last_spdm_request,
+                        libspdm_get_scratch_buffer_last_spdm_request_capacity(context));
+    context->last_spdm_request_size = 0;
+    context->last_spdm_request_session_id_valid = false;
+    return status;
 }
 
 void libspdm_register_get_response_func(void *context, libspdm_get_response_func get_response_func)
